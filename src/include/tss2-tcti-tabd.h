d049d78fc91019c1eb9cac76ba60f716106a49db
Philip Tricca <flihp@twobit.us>
2016-04-07 18:55:14 -0700

    headers: Make include guards consistent.
    Signed-off-by: Philip Tricca <flihp@twobit.us>

--- src/include/tss2-tcti-tabd.h
+++ src/include/tss2-tcti-tabd.h
@@ -1,5 +1,5 @@
-#ifndef TSS2_TCTI_TABD
-#define TSS2_TCTI_TABD
+#ifndef TSS2_TCTI_TABD_H
+#define TSS2_TCTI_TABD_H
 
 #include <tss2/tpm20.h>
 #include <tss2/tss2_tcti.h>
@@ -67,4 +67,4 @@
 
 TSS2_RC tss2_tcti_tabd_init (TSS2_TCTI_CONTEXT *context, size_t *size);
 
-#endif /* TSS2_TCTI_TABD */
+#endif /* TSS2_TCTI_TABD_H */
