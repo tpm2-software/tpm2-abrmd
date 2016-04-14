5a7f75163032bb70200370383bafd52154654019
Philip Tricca <flihp@twobit.us>
2016-04-13 21:57:18 -0700

    session-watcher_unit: Test session callback.
    This test ensures that the file descriptor for a watched session
    creating data is properly passed to the session callback.
    
    Signed-off-by: Philip Tricca <flihp@twobit.us>

--- test/session-watcher_unit.c
+++ test/session-watcher_unit.c
@@ -52,20 +52,22 @@ session_watcher_allocate_teardown (void **state)
  * This is a basic usage flow test. Can it be started, canceled and joined.
  * We're testing the underlying pthread usage ... mostly.
  */
-struct watcher_test_data {
+typedef struct watcher_test_data {
     session_manager_t *manager;
     session_watcher_t *watcher;
+    session_t *session;
     gint wakeup_send_fd;
     gboolean wokeup;
-};
+    gboolean match;
+} watcher_test_data_t;
 
 void
 session_watcher_start_test (void **state)
 {
-    struct watcher_test_data *data;
+    watcher_test_data_t *data;
     gint ret;
 
-    data = (struct watcher_test_data*)*state;
+    data = (watcher_test_data_t*)*state;
     session_watcher_start(data->watcher);
     sleep (1);
     assert_true (data->watcher->running);
@@ -78,10 +80,10 @@ session_watcher_start_test (void **state)
 static void
 session_watcher_start_setup (void **state)
 {
-    struct watcher_test_data *data;
+    watcher_test_data_t *data;
     gint fds[2] = { 0 }, ret;
 
-    data = calloc (1, sizeof (struct watcher_test_data));
+    data = calloc (1, sizeof (watcher_test_data_t));
     data->manager = session_manager_new ();
     if (data->manager == NULL)
         g_error ("failed to allocate new session_manager");
@@ -99,9 +101,9 @@ session_watcher_start_setup (void **state)
 static void
 session_watcher_start_teardown (void **state)
 {
-    struct watcher_test_data *data;
+    watcher_test_data_t *data;
 
-    data = (struct watcher_test_data*)*state;
+    data = (watcher_test_data_t*)*state;
     close (data->watcher->wakeup_receive_fd);
     session_watcher_free (data->watcher);
     session_manager_free (data->manager);
@@ -133,10 +135,10 @@ session_watcher_wakeup_callback (session_watcher_t *watcher,
 static void
 session_watcher_wakeup_setup (void **state)
 {
-    struct watcher_test_data *data;
+    watcher_test_data_t *data;
     int fds[2] = { 0 }, ret;
 
-    data = calloc (1, sizeof (struct watcher_test_data));
+    data = calloc (1, sizeof (watcher_test_data_t));
     data->manager = session_manager_new ();
     if (data->manager == NULL)
         g_error ("failed to allocate new session_manager");
@@ -159,10 +161,10 @@ session_watcher_wakeup_setup (void **state)
 static void
 session_watcher_wakeup_test (void **state)
 {
-    struct watcher_test_data *data;
+    watcher_test_data_t *data;
     gint ret;
 
-    data = (struct watcher_test_data*)*state;
+    data = (watcher_test_data_t*)*state;
     /* This string is arbitrary. */
     ret = write (data->wakeup_send_fd, "hi", strlen ("hi"));
     assert_true (ret != -1);
@@ -179,6 +181,102 @@ session_watcher_wakeup_test (void **state)
     assert_int_equal (ret, 0);
 }
 /* session_watcher_wakeup_test end */
+/* session_watcher_session_callback_test begin
+ * In this test we create a session wather. We then create a new session and
+ * insert it into the session manager being watched. We then signal to the
+ * watcher to indicate that there's a new session to watch. Finally check to
+ * see whether or not our custom call back is invoked by checking a flag
+ * passed as user data that our callback sets.
+ */
+static int
+session_watcher_session_callback_callback (session_watcher_t *watcher,
+                                           gint fd,
+                                           gpointer user_data)
+{
+    session_t *watcher_session, *my_session;
+    gchar buf[256] = { 0 };
+    gint ret = 0;
+
+    my_session = ((watcher_test_data_t*)user_data)->session;
+    watcher_session = session_manager_lookup (watcher->session_manager, fd);
+    assert_non_null (watcher_session);
+    /* This is the test: the session associated with the fd that has data ready
+     * is the same session that we created in the test.
+     */
+    assert_true (watcher_session == my_session);
+    ((watcher_test_data_t*)user_data)->match = TRUE;
+    /* We're not doing anything with the data but we must read from the pipe
+     * or the callback will be invoked repeatedly till we do.
+     */
+    ret = read (fd, buf, 256);
+    assert_true (ret > 0);
+
+    return 1;
+}
+
+static void
+session_watcher_session_callback_setup (void **state)
+{
+    watcher_test_data_t *data;
+    int fds[2] = { 0 }, ret;
+
+    data = calloc (1, sizeof (watcher_test_data_t));
+    if (data == NULL)
+        g_error ("failed to allocate watcher_test_data_t");
+    data->manager = session_manager_new ();
+    if (data->manager == NULL)
+        g_error ("failed to allocate new session_manager");
+    ret = pipe2 (fds, O_CLOEXEC);
+    if (ret != 0)
+        g_error ("failed to get pipe2s");
+    data->match = FALSE;
+    data->watcher = session_watcher_new_full (data->manager,
+                                              fds[0],
+                                              session_watcher_session_callback_callback,
+                                              NULL,
+                                              data);
+    data->wakeup_send_fd = fds[1];
+    if (data->watcher == NULL)
+        g_error ("failed to allocate new session_watcher");
+
+    *state = data;
+}
+
+static void
+session_watcher_session_callback_teardown (void **state)
+{
+    watcher_test_data_t *data = (watcher_test_data_t*)*state;
+
+    session_watcher_cancel (data->watcher);
+    session_watcher_join (data->watcher);
+    close (data->watcher->wakeup_receive_fd);
+    session_watcher_free (data->watcher);
+    session_manager_free (data->manager);
+    close (data->wakeup_send_fd);
+}
+
+
+static void
+session_watcher_session_callback_test (void **state)
+{
+    watcher_test_data_t *data = (watcher_test_data_t*)*state;
+    session_watcher_t *watcher = data->watcher;
+    session_manager_t *manager = data->manager;
+    session_t *session;
+    gint ret, receive_fd, send_fd;
+
+    data->session = session_new (&receive_fd, &send_fd);
+    assert_non_null (data->session);
+    ret = session_watcher_start (watcher);
+    assert_return_code (ret, 0);
+    ret = session_manager_insert (manager, data->session);
+    assert_return_code (ret, 0);
+    ret = write (data->wakeup_send_fd, "wakeup", strlen ("wakeup"));
+    assert_true (ret > 0);
+    sleep (1);
+    assert_true (data->match);
+}
+/* session_watcher_session_callback_test end */
 
 /* session_watcher_session_insert_test begin
  * In this test we create a session watcher and all that that entails. We then
@@ -255,6 +353,9 @@ main (int argc,
         unit_test_setup_teardown (session_watcher_session_insert_test,
                                   session_watcher_session_insert_setup,
                                   session_watcher_start_teardown),
+        unit_test_setup_teardown (session_watcher_session_callback_test,
+                                  session_watcher_session_callback_setup,
+                                  session_watcher_session_callback_teardown),
     };
     return run_tests (tests);
 }
