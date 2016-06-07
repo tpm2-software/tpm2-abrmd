58480ba727c4ed80a1c4d713d274b8d878238edc
Philip Tricca <flihp@twobit.us>
2016-06-06 17:43:38 -0700

    tabd.h: Change DBus names / paths to use Intel.
    Signed-off-by: Philip Tricca <flihp@twobit.us>

--- src/include/tabd.h
+++ src/include/tabd.h
@@ -1,9 +1,9 @@
 #ifndef TSS2_TABD_H
 #define TSS2_TABD_H
 
-#define TAB_DBUS_INTERFACE "us.twobit.tss2.Tab"
-#define TAB_DBUS_NAME "us.twobit.tss2.Tab"
-#define TAB_DBUS_PATH "/us/twobit/tss2/Tab"
+#define TAB_DBUS_INTERFACE "com.intel.tss2.Tab"
+#define TAB_DBUS_NAME "com.intel.tss2.Tab"
+#define TAB_DBUS_PATH "/com/intel/tss2/Tab"
 #define TAB_DBUS_METHOD_CREATE_CONNECTION "CreateConnection"
 #define TAB_DBUS_METHOD_CANCEL "Cancel"
 
