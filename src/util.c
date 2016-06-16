22801426077a583e774b21c410ed2b0afc053b36
Philip Tricca <flihp@twobit.us>
2016-06-16 13:56:22 -0700

    util: Cleanup error handling around realloc, add unit test to mock realloc.
    Signed-off-by: Philip Tricca <flihp@twobit.us>

--- src/util.c
+++ src/util.c
@@ -1,4 +1,5 @@
 #include <errno.h>
+#include <fcntl.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
@@ -45,18 +46,23 @@ read_till_short (const gint   fd,
                  guint8     **buf,
                  size_t      *size)
 {
+    guint8  *local_buf = NULL, *tmp_buf;
     ssize_t bytes_read  = 0;
     size_t  bytes_total = 0;
 
     g_debug ("reading as many bytes from fd %d as I can", fd);
     do {
-        *buf = realloc (*buf, bytes_total + UTIL_BUF_SIZE);
-        if (*buf == NULL)
-            g_error ("realloc of %d bytes failed: %s",
-                     bytes_total + UTIL_BUF_SIZE, strerror (errno));
-        else
-            g_debug ("reallocated buf to %d bytes", bytes_total + UTIL_BUF_SIZE);
-        bytes_read = read (fd, (void*)(*buf + bytes_total), UTIL_BUF_SIZE);
+        g_debug ("reallocing buf at 0x%x to %d bytes", local_buf, bytes_total + UTIL_BUF_SIZE);
+        tmp_buf = realloc (local_buf, bytes_total + UTIL_BUF_SIZE);
+        if (tmp_buf == NULL) {
+            g_warning ("realloc of %d bytes failed: %s",
+                       bytes_total + UTIL_BUF_SIZE, strerror (errno));
+            goto err_out;
+        }
+        g_debug ("reallocated buf at 0x%x to %d bytes", local_buf, bytes_total + UTIL_BUF_SIZE);
+        local_buf = tmp_buf;
+
+        bytes_read = read (fd, (void*)(local_buf + bytes_total), UTIL_BUF_SIZE);
         switch (bytes_read) {
         case -1:
             g_warning ("read failed on fd %d: %s", fd, strerror (errno));
@@ -65,12 +71,19 @@ read_till_short (const gint   fd,
             g_info ("read returned 0 bytes -> fd %d closed", fd);
             break;
         default:
+            g_debug ("read %d bytes from fd %d", bytes_read, fd);
             bytes_total += bytes_read;
             break;
         }
     } while (bytes_read == UTIL_BUF_SIZE && bytes_total < UTIL_BUF_MAX);
     *size = bytes_total;
+    *buf = local_buf;
+
     return bytes_read;
+err_out:
+    if (local_buf != NULL)
+        free (local_buf);
+    return -1;
 }
 
 void
---
+++ test/util-mock-realloc_unit.c
@@ -0,0 +1,66 @@
+#include <errno.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#include <setjmp.h>
+#include <cmocka.h>
+
+#include "util.h"
+
+#define READ_SIZE  UTIL_BUF_SIZE
+#define WRITE_SIZE 10
+
+void*
+__wrap_realloc (void   *buf,
+                size_t  count)
+{
+    errno = (int) mock ();
+    return (void*) mock ();
+}
+
+void
+fail_on_first (void **state)
+{
+    ssize_t read;
+    size_t  total_read;
+    guint8  *buf;
+
+    will_return (__wrap_realloc, ENOMEM);
+    will_return (__wrap_realloc, NULL);
+    read = read_till_short (0, &buf, &total_read);
+    assert_int_equal (read, -1);
+}
+
+/* Cause a failure on the second call to realloc. This requires that we have
+ * one successful round so we must write data to the fd so that the call to
+ * read succeeds.
+ */
+void
+fail_on_second (void **state)
+{
+    ssize_t read;
+    ssize_t total_read;
+    guint8 *buf, *priv_buf;
+    gint fds[2] = {0}, ret = 0;
+
+    ret = pipe (fds);
+    priv_buf = calloc (1, UTIL_BUF_SIZE);
+    will_return (__wrap_realloc, 0);
+    will_return (__wrap_realloc, priv_buf);
+    will_return (__wrap_realloc, ENOMEM);
+    will_return (__wrap_realloc, NULL);
+    write (fds[1], priv_buf, UTIL_BUF_SIZE);
+    read = read_till_short (fds[0], &buf, &total_read);
+    assert_int_equal (read, -1);
+}
+
+gint
+main (gint    argc,
+      gchar  *argv[])
+{
+    const UnitTest tests[] = {
+        unit_test (fail_on_first),
+        unit_test (fail_on_second),
+    };
+    return run_tests (tests);
+}
