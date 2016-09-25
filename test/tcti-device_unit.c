40a06401e2592e12c456c10fd50e4c8d45072700
Philip Tricca <philip.b.tricca@intel.com>
2016-09-24 23:02:09 -0700

    Add unit tests to exercise the initialization functions in TctiDevice and TctiSocket.
    Signed-off-by: Philip Tricca <philip.b.tricca@intel.com>

--- test/tcti-device_unit.c
+++ test/tcti-device_unit.c
@@ -6,22 +6,137 @@
 
 #include "tcti-device.h"
 
+TSS2_RC
+__wrap_InitDeviceTcti (TSS2_TCTI_CONTEXT   *tcti_context,
+                       size_t              *size,
+                       TCTI_DEVICE_CONF    *config)
+{
+    *size = (size_t) mock ();
+    return (TSS2_RC) mock ();
+}
+
+static void
+tcti_device_setup (void **state)
+{
+    *state = tcti_device_new ("test");
+}
+
+static void
+tcti_device_teardown (void **state)
+{
+    TctiDevice *tcti_sock = *state;
+
+    g_object_unref (tcti_sock);
+}
+/**
+ * Test object life cycle: create new object then unref it.
+ */
 static void
 tcti_device_new_unref_test (void **state)
 {
-    TctiDevice *tcti_dev = NULL;
+    TctiDevice *tcti_sock;
 
-    tcti_dev = tcti_device_new ("test");
-    assert_non_null (tcti_dev);
-    g_object_unref (tcti_dev);
+    tcti_device_setup ((void**)&tcti_sock);
+    tcti_device_teardown ((void**)&tcti_sock);
 }
 
+/**
+ * Calling the initialize function causes two calls to InitDeviceTcti. The
+ * first gets the size of the context structure to allocate, the second
+ * does the initialization. Inbetween the function allocates the context.
+ */
+static void
+tcti_device_initialize_success_unit (void **state)
+{
+    Tcti       *tcti      = *state;
+    TctiDevice *tcti_sock = *state;
+    TSS2_RC     rc = TSS2_RC_SUCCESS;
+
+    will_return (__wrap_InitDeviceTcti, 512);
+    will_return (__wrap_InitDeviceTcti, TSS2_RC_SUCCESS);
+
+    will_return (__wrap_InitDeviceTcti, 512);
+    will_return (__wrap_InitDeviceTcti, TSS2_RC_SUCCESS);
+
+    rc = tcti_device_initialize (tcti);
+    assert_int_equal (rc, TSS2_RC_SUCCESS);
+}
+/**
+ * Same as the previous case but this time we use the interface function.
+ */
+static void
+tcti_device_initialize_success_interface_unit (void **state)
+{
+    Tcti       *tcti      = *state;
+    TctiDevice *tcti_sock = *state;
+    TSS2_RC     rc = TSS2_RC_SUCCESS;
+
+    will_return (__wrap_InitDeviceTcti, 512);
+    will_return (__wrap_InitDeviceTcti, TSS2_RC_SUCCESS);
+
+    will_return (__wrap_InitDeviceTcti, 512);
+    will_return (__wrap_InitDeviceTcti, TSS2_RC_SUCCESS);
+
+    rc = tcti_initialize (tcti);
+    assert_int_equal (rc, TSS2_RC_SUCCESS);
+}
+/**
+ * Cause first call to InitDeviceTcti in tcti_device_initialize to fail.
+ * We should get the RC that we provide in the second will_return sent
+ * back to us.
+ */
+static void
+tcti_device_initialize_fail_on_first_init_unit (void **state)
+{
+    Tcti       *tcti      = *state;
+    TctiDevice *tcti_sock = *state;
+    TSS2_RC     rc        = TSS2_RC_SUCCESS;
+
+    will_return (__wrap_InitDeviceTcti, 0);
+    will_return (__wrap_InitDeviceTcti, TSS2_TCTI_RC_GENERAL_FAILURE);
+
+    rc = tcti_device_initialize (tcti);
+    assert_int_equal (rc, TSS2_TCTI_RC_GENERAL_FAILURE);
+}
+/**
+ * Cause the second call to InitDeviceTcti in the tcti_device_initialize to
+ * fail. We should get the RC taht we provide in the 4th will_return sent
+ * back to us.
+ */
+static void
+tcti_device_initialize_fail_on_second_init_unit (void **state)
+{
+    Tcti       *tcti      = *state;
+    TctiDevice *tcti_sock = *state;
+    TSS2_RC     rc = TSS2_RC_SUCCESS;
+
+    will_return (__wrap_InitDeviceTcti, 512);
+    will_return (__wrap_InitDeviceTcti, TSS2_RC_SUCCESS);
+
+    will_return (__wrap_InitDeviceTcti, 0);
+    will_return (__wrap_InitDeviceTcti, TSS2_TCTI_RC_GENERAL_FAILURE);
+
+    rc = tcti_device_initialize (tcti);
+    assert_int_equal (rc, TSS2_TCTI_RC_GENERAL_FAILURE);
+}
 gint
 main (gint     argc,
       gchar   *argv[])
 {
     const UnitTest tests[] = {
         unit_test (tcti_device_new_unref_test),
+        unit_test_setup_teardown (tcti_device_initialize_success_unit,
+                                  tcti_device_setup,
+                                  tcti_device_teardown),
+        unit_test_setup_teardown (tcti_device_initialize_success_interface_unit,
+                                  tcti_device_setup,
+                                  tcti_device_teardown),
+        unit_test_setup_teardown (tcti_device_initialize_fail_on_first_init_unit,
+                                  tcti_device_setup,
+                                  tcti_device_teardown),
+        unit_test_setup_teardown (tcti_device_initialize_fail_on_second_init_unit,
+                                  tcti_device_setup,
+                                  tcti_device_teardown),
     };
     return run_tests (tests);
 }
--- test/tcti-socket_unit.c
+++ test/tcti-socket_unit.c
@@ -6,22 +6,138 @@
 
 #include "tcti-socket.h"
 
+TSS2_RC
+__wrap_InitSocketTcti (TSS2_TCTI_CONTEXT *tcti_context,
+                       size_t *size,
+                       TCTI_SOCKET_CONF *config,
+                       uint8_t serverSockets)
+{
+    *size = (size_t) mock ();
+    return (TSS2_RC) mock ();
+}
+
+static void
+tcti_socket_setup (void **state)
+{
+    *state = tcti_socket_new ("test", 10);
+}
+
+static void
+tcti_socket_teardown (void **state)
+{
+    TctiSocket *tcti_sock = *state;
+
+    g_object_unref (tcti_sock);
+}
+/**
+ * Test object life cycle: create new object then unref it.
+ */
 static void
 tcti_socket_new_unref_test (void **state)
 {
-    TctiSocket *tcti_sock = NULL;
+    TctiSocket *tcti_sock;
 
-    tcti_sock = tcti_socket_new ("test", 10);
-    assert_non_null (tcti_sock);
-    g_object_unref (tcti_sock);
+    tcti_socket_setup ((void**)&tcti_sock);
+    tcti_socket_teardown ((void**)&tcti_sock);
 }
 
+/**
+ * Calling the initialize function causes two calls to InitSocketTcti. The
+ * first gets the size of the context structure to allocate, the second
+ * does the initialization. Inbetween the function allocates the context.
+ */
+static void
+tcti_socket_initialize_success_unit (void **state)
+{
+    Tcti       *tcti      = *state;
+    TctiSocket *tcti_sock = *state;
+    TSS2_RC     rc = TSS2_RC_SUCCESS;
+
+    will_return (__wrap_InitSocketTcti, 512);
+    will_return (__wrap_InitSocketTcti, TSS2_RC_SUCCESS);
+
+    will_return (__wrap_InitSocketTcti, 512);
+    will_return (__wrap_InitSocketTcti, TSS2_RC_SUCCESS);
+
+    rc = tcti_socket_initialize (tcti);
+    assert_int_equal (rc, TSS2_RC_SUCCESS);
+}
+/**
+ * Same as the previous case but this time we use the interface function.
+ */
+static void
+tcti_socket_initialize_success_interface_unit (void **state)
+{
+    Tcti       *tcti      = *state;
+    TctiSocket *tcti_sock = *state;
+    TSS2_RC     rc = TSS2_RC_SUCCESS;
+
+    will_return (__wrap_InitSocketTcti, 512);
+    will_return (__wrap_InitSocketTcti, TSS2_RC_SUCCESS);
+
+    will_return (__wrap_InitSocketTcti, 512);
+    will_return (__wrap_InitSocketTcti, TSS2_RC_SUCCESS);
+
+    rc = tcti_initialize (tcti);
+    assert_int_equal (rc, TSS2_RC_SUCCESS);
+}
+/**
+ * Cause first call to InitSocketTcti in tcti_socket_initialize to fail.
+ * We should get the RC that we provide in the second will_return sent
+ * back to us.
+ */
+static void
+tcti_socket_initialize_fail_on_first_init_unit (void **state)
+{
+    Tcti       *tcti      = *state;
+    TctiSocket *tcti_sock = *state;
+    TSS2_RC     rc        = TSS2_RC_SUCCESS;
+
+    will_return (__wrap_InitSocketTcti, 0);
+    will_return (__wrap_InitSocketTcti, TSS2_TCTI_RC_GENERAL_FAILURE);
+
+    rc = tcti_socket_initialize (tcti);
+    assert_int_equal (rc, TSS2_TCTI_RC_GENERAL_FAILURE);
+}
+/**
+ * Cause the second call to InitSocketTcti in the tcti_socket_initialize to
+ * fail. We should get the RC taht we provide in the 4th will_return sent
+ * back to us.
+ */
+static void
+tcti_socket_initialize_fail_on_second_init_unit (void **state)
+{
+    Tcti       *tcti      = *state;
+    TctiSocket *tcti_sock = *state;
+    TSS2_RC     rc = TSS2_RC_SUCCESS;
+
+    will_return (__wrap_InitSocketTcti, 512);
+    will_return (__wrap_InitSocketTcti, TSS2_RC_SUCCESS);
+
+    will_return (__wrap_InitSocketTcti, 0);
+    will_return (__wrap_InitSocketTcti, TSS2_TCTI_RC_GENERAL_FAILURE);
+
+    rc = tcti_socket_initialize (tcti);
+    assert_int_equal (rc, TSS2_TCTI_RC_GENERAL_FAILURE);
+}
 gint
 main (gint     argc,
       gchar   *argv[])
 {
     const UnitTest tests[] = {
         unit_test (tcti_socket_new_unref_test),
+        unit_test_setup_teardown (tcti_socket_initialize_success_unit,
+                                  tcti_socket_setup,
+                                  tcti_socket_teardown),
+        unit_test_setup_teardown (tcti_socket_initialize_success_interface_unit,
+                                  tcti_socket_setup,
+                                  tcti_socket_teardown),
+        unit_test_setup_teardown (tcti_socket_initialize_fail_on_first_init_unit,
+                                  tcti_socket_setup,
+                                  tcti_socket_teardown),
+        unit_test_setup_teardown (tcti_socket_initialize_fail_on_second_init_unit,
+                                  tcti_socket_setup,
+                                  tcti_socket_teardown),
     };
     return run_tests (tests);
 }
