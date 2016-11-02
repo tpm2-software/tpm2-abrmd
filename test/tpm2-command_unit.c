7f577bcc1f69963eaa9ec3cd5152927d05372fb9
Philip Tricca <philip.b.tricca@intel.com>
2016-11-02 15:49:29 -0700

    Tpm2Command: Add unit tests for the 'get' functions (tag, size, command code)
    Signed-off-by: Philip Tricca <philip.b.tricca@intel.com>

--- test/tpm2-command_unit.c
+++ test/tpm2-command_unit.c
@@ -79,6 +79,102 @@ tpm2_command_type_check_test (void **state)
     g_object_unref (cmd);
 }
 
+static void
+tpm2_command_get_buffer_test (void **state)
+{
+    Tpm2Command     *cmd     = NULL;
+    guint8          *buffer  = NULL, *buffer_ret = NULL;
+    guint32          size    = 10;
+    SessionData     *session = NULL;
+
+    buffer = calloc (1, size);
+
+    cmd = tpm2_command_new (session, size, buffer);
+    buffer_ret = tpm2_command_get_buffer (cmd);
+    assert_int_equal (buffer,  buffer_ret);
+
+    g_object_unref (cmd);
+}
+
+static void
+tpm2_command_get_tag_test (void **state)
+{
+    Tpm2Command         *cmd       = NULL;
+    guint32              size      = 2;
+    SessionData         *session   = NULL;
+    guint8              *buffer    = NULL;
+    TPMI_ST_COMMAND_TAG  tag_ret;
+
+    buffer = calloc (1, size);
+    /* this is tpm_st_sessions in network byte order */
+    buffer[0] = 0x80;
+    buffer[1] = 0x02;
+
+    cmd = tpm2_command_new (session, size, buffer);
+    tag_ret = tpm2_command_get_tag (cmd);
+    assert_int_equal (tag_ret, TPM_ST_SESSIONS);
+
+    g_object_unref (cmd);
+}
+
+static void
+tpm2_command_get_size_test (void **state)
+{
+    Tpm2Command         *cmd       = NULL;
+    guint32              size      = 6, size_ret = 0;
+    SessionData         *session   = NULL;
+    /* this is TPM_ST_SESSIONS + a size of 0x6 in network byte order */
+    guint8              *buffer    = NULL;
+
+    buffer = calloc (1, size);
+    /* this is tpm_st_sessions in network byte order */
+    buffer[0] = 0x80;
+    buffer[1] = 0x02;
+    buffer[2] = 0x00;
+    buffer[3] = 0x00;
+    buffer[4] = 0x00;
+    buffer[5] = 0x06;
+
+    cmd = tpm2_command_new (session, size, buffer);
+    size_ret = tpm2_command_get_size (cmd);
+    assert_int_equal (size_ret, size);
+
+    g_object_unref (cmd);
+}
+
+static void
+tpm2_command_get_code_test (void **state)
+{
+    Tpm2Command    *cmd       = NULL;
+    guint32         size      = 10;
+    SessionData    *session   = NULL;
+    guint8         *buffer    = NULL;
+    TPM_CC         command_code;
+
+    buffer = calloc (1, size);
+    /**
+     * This is TPM_ST_SESSIONS + a size of 0x0a + the command code for
+     * GetCapability in network byte order
+     */
+    buffer[0] = 0x80;
+    buffer[1] = 0x02;
+    buffer[2] = 0x00;
+    buffer[3] = 0x00;
+    buffer[4] = 0x00;
+    buffer[5] = 0x06;
+    buffer[6] = 0x00;
+    buffer[7] = 0x00;
+    buffer[8] = 0x01;
+    buffer[9] = 0x7a;
+
+    cmd = tpm2_command_new (session, size, buffer);
+    command_code = tpm2_command_get_code (cmd);
+    assert_int_equal (command_code, TPM_CC_GetCapability);
+
+    g_object_unref (cmd);
+
+}
+
 gint
 main (gint    argc,
       gchar  *argv[])
@@ -88,6 +184,10 @@ main (gint    argc,
         unit_test (tpm2_command_new_unref_with_buffer_test),
         unit_test (tpm2_command_new_unref_with_session_test),
         unit_test (tpm2_command_type_check_test),
+        unit_test (tpm2_command_get_buffer_test),
+        unit_test (tpm2_command_get_tag_test),
+        unit_test (tpm2_command_get_size_test),
+        unit_test (tpm2_command_get_code_test),
     };
     return run_tests (tests);
 }
