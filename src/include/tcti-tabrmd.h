50bb32ea8824133c0dbea24be8fdea3813d9a9c1
Philip Tricca <philip.b.tricca@intel.com>
2016-12-15 21:52:50 -0800

    tcti-tabrmd.h: Fixup header to get init function name right.
    Signed-off-by: Philip Tricca <philip.b.tricca@intel.com>

--- src/include/tcti-tabrmd.h
+++ src/include/tcti-tabrmd.h
@@ -4,6 +4,6 @@
 #include <sapi/tpm20.h>
 #include <sapi/tss2_tcti.h>
 
-TSS2_RC tss2_tcti_tabd_init (TSS2_TCTI_CONTEXT *context, size_t *size);
+TSS2_RC tss2_tcti_tabrmd_init (TSS2_TCTI_CONTEXT *context, size_t *size);
 
 #endif /* TSS2_TCTI_TABD_H */
