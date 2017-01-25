0ab498dc2cac370b272b29ed78edf5555444c699
Philip Tricca <philip.b.tricca@intel.com>
2017-01-24 19:26:43 -0800

    Add script to run tpm2-abrmd under valgrind memcheck.
    This is to catch memory leaks. The program driving the tpm2-abrmd when
    it's running will largely determin the test case.
    
    Signed-off-by: Philip Tricca <philip.b.tricca@intel.com>

---
+++ scripts/tpm2-abrmd_memcheck.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+G_DEBUG=fatal-criticals,gc-friendly \
+valgrind --tool=memcheck --show-leak-kinds=definite,indirect \
+         --leak-check=full \
+tpm2-abrmd --fail-on-loaded-trans --tcti=socket
