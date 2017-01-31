40a84d6afed3d1a1388b938a9f9aaf328b7cad95
Philip Tricca <philip.b.tricca@intel.com>
2017-01-31 12:14:27 -0800

    Move create_primary, create_key and load_key functions to libtest.
    This will be required by new integration tests as they're written.
    
    Signed-off-by: Philip Tricca <philip.b.tricca@intel.com>

--- test/integration/common.h
+++ test/integration/common.h
@@ -14,3 +14,26 @@ sapi_context_init (
     TSS2_SYS_CONTEXT    **sapi_context,
     TSS2_TCTI_CONTEXT    *tcti_context
     );
+
+TSS2_RC
+create_primary (
+    TSS2_SYS_CONTEXT *sapi_context,
+    TPM_HANDLE       *handle
+    );
+
+TSS2_RC
+create_key (
+    TSS2_SYS_CONTEXT *sapi_context,
+    TPM_HANDLE        parent_handle,
+    TPM2B_PRIVATE    *out_private,
+    TPM2B_PUBLIC     *out_public
+    );
+
+TSS2_RC
+load_key (
+    TSS2_SYS_CONTEXT *sapi_context,
+    TPM_HANDLE        parent_handle,
+    TPM_HANDLE       *out_handle,
+    TPM2B_PRIVATE    *in_private,
+    TPM2B_PUBLIC     *in_public
+    );
