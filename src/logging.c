tpm2-software/tpm2-abrmd

commit df589f2829dcb92a926b87a56c552492c6100552

Fix implicit sign conversions.
I'd like to be able to add -Wconversion to the CFLAGS but the code
generated by the gdbus-codegen tool doesn't play well with this flag. It
may be possible to do some magic to disable this flag for that single
compilation unit but that will probably require compiling the code into a
separate utility library just as a way to allow filtering the flag out.

Signed-off-by: Philip Tricca <philip.b.tricca@intel.com>

src/logging.c
@@ -48,11 +48,12 @@ set_logger (gchar *name)
 {
    if (g_strcmp0 (name, "syslog") == 0) {
         g_info ("logging to syslog");
-        return g_log_set_handler (NULL,
-                                  LOG_LEVEL_ALL | G_LOG_FLAG_FATAL | \
-                                  G_LOG_FLAG_RECURSION,
-                                  syslog_log_handler,
-                                  NULL);
+        g_log_set_handler (NULL,
+                           LOG_LEVEL_ALL | G_LOG_FLAG_FATAL | \
+                           G_LOG_FLAG_RECURSION,
+                           syslog_log_handler,
+                           NULL);
+        return 0;
     }
-    return 0;
+    return -1;
 }

src/access-broker.c
@@ -521,10 +521,15 @@ access_broker_get_trans_object_count (AccessBroker *broker,
     if (broker == NULL || count == NULL)
         g_error ("get_loaded_transient_object_count: got NULL parameter");
     sapi_context = access_broker_lock_sapi (broker);
+    /*
+     * GCC gets confused by the TRANSIENT_FIRST constant being used for the
+     * 4th parameter. It assumes that it's a signed type which causes
+     * -Wsign-conversion to complain. Casting to UINT32 is all we can do.
+     */
     rc = Tss2_Sys_GetCapability (sapi_context,
                                  NULL,
                                  TPM_CAP_HANDLES,
-                                 TRANSIENT_FIRST,
+                                 (UINT32)TRANSIENT_FIRST,
                                  TRANSIENT_LAST - TRANSIENT_FIRST,
                                  &more_data,
                                  &capability_data,

src/command-attrs.c
@@ -90,10 +90,10 @@ command_attrs_init (CommandAttrs *attrs,
                                  NULL);
     if (rc != TSS2_RC_SUCCESS) {
         g_warning ("failed to get TPM command attributes: 0x%" PRIx32, rc);
-        rc = access_broker_unlock (broker);
+        access_broker_unlock (broker);
         return -1;
     }
-    rc = access_broker_unlock (broker);
+    access_broker_unlock (broker);
 
     attrs->count = capability_data.data.command.count;
     g_debug ("got attributes for 0x%" PRIx32 " commands", attrs->count);

src/command-source.c
@@ -129,7 +129,7 @@ command_source_on_new_session (SessionManager   *session_manager,
                                SessionData      *session_data,
                                CommandSource    *command_source)
 {
-    gint ret;
+    ssize_t ret;
 
     g_debug ("command_source_on_new_session: writing \"%s\" to fd: %d",
              WAKEUP_DATA, command_source->wakeup_send_fd);
@@ -300,14 +300,16 @@ command_source_session_responder (CommandSource      *source,
     return TRUE;
 }
 
-int
+ssize_t
 wakeup_responder (CommandSource *source)
 {
     g_debug ("Got new session, updating fd_set");
     char buf[3] = { 0 };
-    gint ret = read (source->wakeup_receive_fd, buf, WAKEUP_SIZE);
+    ssize_t ret;
+
+    ret = read (source->wakeup_receive_fd, buf, WAKEUP_SIZE);
     if (ret != WAKEUP_SIZE)
-        g_error ("read on wakeup_receive_fd returned %d, was expecting %d",
+        g_error ("read on wakeup_receive_fd returned %zd, was expecting %d",
                  ret, WAKEUP_SIZE);
     return ret;
 }

src/handle-map-entry.c
@@ -26,10 +26,10 @@ handle_map_entry_get_property (GObject        *object,
 
     switch (property_id) {
     case PROP_PHANDLE:
-        g_value_set_uint (value, (gint)self->phandle);
+        g_value_set_uint (value, (guint)self->phandle);
         break;
     case PROP_VHANDLE:
-        g_value_set_uint (value, (gint)self->vhandle);
+        g_value_set_uint (value, (guint)self->vhandle);
         break;
     case PROP_CONTEXT:
         g_value_set_pointer (value, &self->context);

src/handle-map.c
@@ -49,7 +49,7 @@ handle_map_set_property (GObject        *object,
 
     switch (property_id) {
     case PROP_HANDLE_TYPE:
-        map->handle_type = g_value_get_uint (value);
+        map->handle_type = (TPM_HT)g_value_get_uint (value);
         break;
     case PROP_MAX_ENTRIES:
         map->max_entries = g_value_get_uint (value);
@@ -314,7 +314,7 @@ handle_map_next_vhandle (HandleMap *map)
     /* (2 ^ 24) - 1 handles max, return 0 if we roll over*/
     if (map->handle_count & HR_RANGE_MASK)
         return 0;
-    handle = map->handle_count + (map->handle_type << HR_SHIFT);
+    handle = map->handle_count + (TPM_HANDLE)(map->handle_type << HR_SHIFT);
     ++map->handle_count;
     return handle;
 }

src/resource-manager.c
@@ -69,7 +69,7 @@ resource_manager_load_contexts (ResourceManager *resmgr,
     SessionData  *session;
     TSS2_RC       rc;
     TPM_HANDLE    handles[3] = { 0, };
-    guint i, handle_count;;
+    guint8 i, handle_count;;
 
     g_debug ("resource_manager_load_contexts");
     if (!resmgr || !command || !entries || !entry_count) {

src/response-sink.c
@@ -202,7 +202,7 @@ response_sink_process_response (Tpm2Response *response)
     guint32      size    = tpm2_response_get_size (response);
     guint8      *buffer  = tpm2_response_get_buffer (response);
     SessionData *session = tpm2_response_get_session (response);
-    guint        fd      = session_data_send_fd (session);
+    gint         fd      = session_data_send_fd (session);
 
     g_debug ("response_sink_thread got response: 0x%" PRIxPTR " size %d",
              (uintptr_t)response, size);

commit 9c3a6abf24d1677373d295b07f226d2b90fb32bd

logging: Fix bug introduced by df589f2829dc.
Fixing a bug in the set_logger function introduced another. The default
case of "stdout" wasn't being handled in the prevous code and the
default simply caused a successful return value. The chagen from
df589f2829dc caused the default case to return an error instead.

This change handles the default 'stdout' case just like the 'syslog'
case now and 0 is returned. -1 is returned when an unkonwn logger is
encountered.

Signed-off-by: Philip Tricca <philip.b.tricca@intel.com>

src/logging.c
@@ -54,6 +54,10 @@ set_logger (gchar *name)
                            syslog_log_handler,
                            NULL);
         return 0;
+    } else if (g_strcmp0 (name, "stdout") == 0) {
+        /* stdout is the default for g_log, nothing to do but return 0 */
+        g_info ("logging to stdout");
+        return 0;
     }
     return -1;
 }