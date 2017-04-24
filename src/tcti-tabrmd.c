tpm2-software/tpm2-abrmd

commit a62c1f9acddecb6874597acb4da0da4a900e932e

Added more NULL checks.

src/tcti-tabrmd.c
@@ -50,8 +50,11 @@ tss2_tcti_tabrmd_transmit (TSS2_TCTI_CONTEXT *context,
     TSS2_RC tss2_ret = TSS2_RC_SUCCESS;
 
     g_debug ("tss2_tcti_tabrmd_transmit");
-    if ((context == NULL) || (size == 0) || (command == NULL)) {
-      return TSS2_TCTI_RC_BAD_VALUE;
+    if (context == NULL) {
+        return TSS2_TCTI_RC_BAD_CONTEXT;
+    }
+    if ((size == 0) || (command == NULL)) {
+        return TSS2_TCTI_RC_BAD_VALUE;
     }
     if (TSS2_TCTI_MAGIC (context) != TSS2_TCTI_TABRMD_MAGIC ||
         TSS2_TCTI_VERSION (context) != TSS2_TCTI_TABRMD_VERSION) {
@@ -175,8 +178,11 @@ tss2_tcti_tabrmd_receive (TSS2_TCTI_CONTEXT *context,
     size_t ret = 0;
 
     g_debug ("tss2_tcti_tabrmd_receive");
-    if ((context == NULL) || (size == 0) || (response == NULL)) {
-      return TSS2_TCTI_RC_BAD_VALUE;
+    if (context == NULL) {
+        return TSS2_TCTI_RC_BAD_CONTEXT;
+    }
+    if ((size == 0) || (response == NULL)) {
+        return TSS2_TCTI_RC_BAD_VALUE;
     }
     if (TSS2_TCTI_MAGIC (context) != TSS2_TCTI_TABRMD_MAGIC ||
         TSS2_TCTI_VERSION (context) != TSS2_TCTI_TABRMD_VERSION) {
@@ -258,6 +264,10 @@ tss2_tcti_tabrmd_finalize (TSS2_TCTI_CONTEXT *context)
     int ret = 0;
 
     g_debug ("tss2_tcti_tabrmd_finalize");
+    if (context == NULL) {
+        g_warning ("Invalid parameter");
+        return;
+    }
     if (TSS2_TCTI_TABRMD_FD_RECEIVE (context) != 0) {
         ret = close (TSS2_TCTI_TABRMD_FD_RECEIVE (context));
         TSS2_TCTI_TABRMD_FD_RECEIVE (context) = 0;
@@ -284,6 +294,9 @@ tss2_tcti_tabrmd_cancel (TSS2_TCTI_CONTEXT *context)
     gboolean cancel_ret;
 
     g_info ("tss2_tcti_tabrmd_cancel");
+    if (context == NULL) {
+        return TSS2_TCTI_RC_BAD_CONTEXT;
+    }
     if (TSS2_TCTI_TABRMD_STATE (context) != TABRMD_STATE_RECEIVE) {
         return TSS2_TCTI_RC_BAD_SEQUENCE;
     }
@@ -307,6 +320,9 @@ tss2_tcti_tabrmd_get_poll_handles (TSS2_TCTI_CONTEXT     *context,
                                    TSS2_TCTI_POLL_HANDLE *handles,
                                    size_t                *num_handles)
 {
+    if (context == NULL) {
+        return TSS2_TCTI_RC_BAD_CONTEXT;
+    }
     if (num_handles == NULL) {
         return TSS2_TCTI_RC_BAD_REFERENCE;
     }
@@ -329,6 +345,9 @@ tss2_tcti_tabrmd_set_locality (TSS2_TCTI_CONTEXT *context,
     GError *error = NULL;
 
     g_info ("tss2_tcti_tabrmd_set_locality");
+    if (context == NULL) {
+        return TSS2_TCTI_RC_BAD_CONTEXT;
+    }
     if (TSS2_TCTI_TABRMD_STATE (context) != TABRMD_STATE_TRANSMIT) {
         return TSS2_TCTI_RC_BAD_SEQUENCE;
     }

commit 3e89f869a9e5000bfa850c850bbbb58f0dc51649

tctis: Fix response codes for context == NULL conditions and related.
After 294d3d099f4c2bf295a3df7d210d4c49ff02d984 and
a62c1f9acddecb6874597acb4da0da4a900e932e broke a hand full of the unit
tests for the TCTIs I went back to the spec to see what these things
should be. Turns out there's some inconsistency in the spec and the
upstream TSS repo got it wrong.

This patch gets these values right. There's a patch pending for the TSS
that fixes the upstream bug. Unit tests here will remain broken till
it's merged.

Signed-off-by: Philip Tricca <philip.b.tricca@intel.com>

src/tcti-tabrmd.c
@@ -50,10 +50,10 @@ tss2_tcti_tabrmd_transmit (TSS2_TCTI_CONTEXT *context,
     TSS2_RC tss2_ret = TSS2_RC_SUCCESS;
 
     g_debug ("tss2_tcti_tabrmd_transmit");
-    if (context == NULL) {
-        return TSS2_TCTI_RC_BAD_CONTEXT;
+    if (context == NULL || command == NULL) {
+        return TSS2_TCTI_RC_BAD_REFERENCE;
     }
-    if ((size == 0) || (command == NULL)) {
+    if (size == 0) {
         return TSS2_TCTI_RC_BAD_VALUE;
     }
     if (TSS2_TCTI_MAGIC (context) != TSS2_TCTI_TABRMD_MAGIC ||
@@ -178,10 +178,10 @@ tss2_tcti_tabrmd_receive (TSS2_TCTI_CONTEXT *context,
     size_t ret = 0;
 
     g_debug ("tss2_tcti_tabrmd_receive");
-    if (context == NULL) {
-        return TSS2_TCTI_RC_BAD_CONTEXT;
+    if (context == NULL || size == NULL) {
+        return TSS2_TCTI_RC_BAD_REFERENCE;
     }
-    if ((size == 0) || (response == NULL)) {
+    if (response == NULL && *size != 0) {
         return TSS2_TCTI_RC_BAD_VALUE;
     }
     if (TSS2_TCTI_MAGIC (context) != TSS2_TCTI_TABRMD_MAGIC ||

test/tss2-tcti-echo_unit.c
@@ -237,7 +237,7 @@ tss2_tcti_echo_transmit_null_context_unit (void **state)
     TSS2_RC rc;
 
     rc = tss2_tcti_transmit (NULL, 0, NULL);
-    assert_int_equal (rc, TSS2_TCTI_RC_BAD_CONTEXT);
+    assert_int_equal (rc, TSS2_TCTI_RC_BAD_REFERENCE);
 }
 /**
  * A test: Call transmit with a NULL command buffer. Ensure we get the
@@ -337,7 +337,7 @@ tss2_tcti_echo_receive_null_context_unit (void **state)
     TSS2_RC rc;
 
     rc = tss2_tcti_receive (NULL, NULL, NULL, 0);
-    assert_int_equal (rc, TSS2_TCTI_RC_BAD_CONTEXT);
+    assert_int_equal (rc, TSS2_TCTI_RC_BAD_REFERENCE);
 }
 /**
  * A test: Call receive with a NULL size. Ensure we get the right error

test/tss2-tcti-tabrmd_unit.c
@@ -262,7 +262,7 @@ tcti_tabrmd_transmit_null_context_test (void **state)
     TSS2_RC rc;
 
     rc = tss2_tcti_transmit (NULL, 5, NULL);
-    assert_int_equal (rc, TSS2_TCTI_RC_BAD_CONTEXT);
+    assert_int_equal (rc, TSS2_TCTI_RC_BAD_REFERENCE);
 }
 static void
 tcti_tabrmd_receive_null_context_test (void **state)
@@ -270,7 +270,7 @@ tcti_tabrmd_receive_null_context_test (void **state)
     TSS2_RC rc;
 
     rc = tss2_tcti_receive (NULL, NULL, NULL, TSS2_TCTI_TIMEOUT_BLOCK);
-    assert_int_equal (rc, TSS2_TCTI_RC_BAD_CONTEXT);
+    assert_int_equal (rc, TSS2_TCTI_RC_BAD_REFERENCE);
 }
 static void
 tcti_tabrmd_cancel_null_context_test (void **state)
@@ -278,7 +278,7 @@ tcti_tabrmd_cancel_null_context_test (void **state)
     TSS2_RC rc;
 
     rc = tss2_tcti_cancel (NULL);
-    assert_int_equal (rc, TSS2_TCTI_RC_BAD_CONTEXT);
+    assert_int_equal (rc, TSS2_TCTI_RC_BAD_REFERENCE);
 }
 static void
 tcti_tabrmd_get_poll_handles_null_context_test (void **state)
@@ -286,7 +286,7 @@ tcti_tabrmd_get_poll_handles_null_context_test (void **state)
     TSS2_RC rc;
 
     rc = tss2_tcti_get_poll_handles (NULL, NULL, NULL);
-    assert_int_equal (rc, TSS2_TCTI_RC_BAD_CONTEXT);
+    assert_int_equal (rc, TSS2_TCTI_RC_BAD_REFERENCE);
 }
 static void
 tcti_tabrmd_set_locality_null_context_test (void **state)
@@ -294,7 +294,7 @@ tcti_tabrmd_set_locality_null_context_test (void **state)
     TSS2_RC rc;
 
     rc = tss2_tcti_set_locality (NULL, 5);
-    assert_int_equal (rc, TSS2_TCTI_RC_BAD_CONTEXT);
+    assert_int_equal (rc, TSS2_TCTI_RC_BAD_REFERENCE);
 }
 /*
  * This test makes a single call to the transmit function in the the
@@ -719,7 +719,7 @@ tcti_tabrmd_receive_null_response_size_nonzero_test (void **state)
                             &size,
                             NULL,
                             TSS2_TCTI_TIMEOUT_BLOCK);
-    assert_int_equal (rc, TSS2_TCTI_RC_BAD_REFERENCE);
+    assert_int_equal (rc, TSS2_TCTI_RC_BAD_VALUE);
 }
 /*
  * If response buffer size is smaller than the header size buffer is