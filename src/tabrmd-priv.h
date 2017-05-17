69e3de9c38fe7991031d20a47fe1330a707ae0be
Philip Tricca <philip.b.tricca@intel.com>
2017-05-17 12:02:10 -0700

    tabrmd-priv: Define G_OPTION_FLAG_NONE if not already defined.
    This constant is defined in glib-2.0 versions >= 2.42. Travis CI
    packages a super old version of Ubuntu that provides glib-2.0 version
    2.40. So we define this ourselves if it's not already defined for us.
    
    Signed-off-by: Philip Tricca <philip.b.tricca@intel.com>

--- src/tabrmd-priv.h
+++ src/tabrmd-priv.h
@@ -34,6 +34,10 @@
 #define TABD_INIT_THREAD_NAME "tss2-tabrmd_init-thread"
 #define TABD_RAND_FILE "/dev/random"
 
+#ifndef G_OPTION_FLAG_NONE
+#define G_OPTION_FLAG_NONE 0
+#endif
+
 typedef struct tabrmd_options {
     GBusType        bus;
     TctiOptions    *tcti_options;
