a9704d849a5a1388a12089432d88d6a48654fec5
Philip Tricca <philip.b.tricca@intel.com>
2017-08-20 19:14:25 -0700

    test: Remove unused test code.
    This stuff was early test code that's been replaced by the integration
    test harness.
    
    Signed-off-by: Philip Tricca <philip.b.tricca@intel.com>

--- scripts/tpm2-abrmd_memcheck.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-sudo -u tss \
-G_DEBUG=fatal-criticals,gc-friendly \
-valgrind --tool=memcheck --show-leak-kinds=definite,indirect \
-         --leak-check=full \
-./src/tpm2-abrmd --fail-on-loaded-trans --tcti=socket
