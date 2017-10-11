4ace4f507da55633c121c93416b2d7872b3ef8cf
Philip Tricca <philip.b.tricca@intel.com>
2017-10-11 11:41:13 -0700

    CommandAttrs: Use g_clear_pointer in finalize instead of free.
    This saves us the explicit NULL test and aligns with GObject best
    practices.
    
    Signed-off-by: Philip Tricca <philip.b.tricca@intel.com>

--- src/command-attrs.c
+++ src/command-attrs.c
@@ -46,8 +46,7 @@ command_attrs_finalize (GObject *obj)
     CommandAttrs *attrs = COMMAND_ATTRS (obj);
 
     g_debug ("command_attrs_finalize: 0x%" PRIxPTR, (uintptr_t)attrs);
-    if (attrs->command_attrs)
-        free (attrs->command_attrs);
+    g_clear_pointer (&attrs->command_attrs, g_free);
     G_OBJECT_CLASS (command_attrs_parent_class)->finalize (obj);
 }
 
