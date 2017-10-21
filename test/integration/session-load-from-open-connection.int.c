8bde44ed2f8eccea38f153ca20b31b95fd75787b
Philip Tricca <philip.b.tricca@intel.com>
2017-10-21 08:59:33 -0700

    test: Add integration test to create and load sessions over different connections.
    The use case behind this feature (and the break in connectionisolation it requires)
    is a pipeline or collaborating processes with one creating the session, another using
    it. This test explicitly covers the case where the connection that created the
    session is still active when a saved session is loaded from another connection.
    
    Access control in this case is outside the scope of the tabrmd. To load the session
    from the second connection requires access to the saved context blob (integrity
    protected by the TPM). Access to this blob must be controlled by the application
    that saved it. Simple filesystem DAC will likely be sufficient for most but use of
    SELinux policy is advised.
    
    Signed-off-by: Philip Tricca <philip.b.tricca@intel.com>

---
+++ test/integration/session-load-from-open-connection.int.c
@@ -0,0 +1,101 @@
+/*
+ * Copyright (c) 2017, Intel Corporation
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
+ * THE POSSIBILITY OF SUCH DAMAGE.
+ */
+#include <inttypes.h>
+#include <glib.h>
+#include <stdio.h>
+
+#include "sapi/tpm20.h"
+
+#include "common.h"
+#include "test-options.h"
+#include "context-util.h"
+
+/*
+ * This test exercises the session tracking logic, specifically the ability
+ * / feature that allows a session created and saved over one connection to
+ * the tabrmd to be loaded from another connection.
+ * This test creates an auth session using "connection0", saves it and then
+ * loads it from "connection1" *before* "connection0" is closed.
+ */
+int
+main (int argc,
+      char *argv[])
+{
+    TSS2_RC rc;
+    TSS2_SYS_CONTEXT *sapi_context0, *sapi_context1;
+    TPMI_SH_AUTH_SESSION  session_handle = 0, session_handle_load = 0;
+    TPMS_CONTEXT          context = { 0, };
+    test_opts_t opts = {
+        .tcti_type      = TCTI_DEFAULT,
+        .device_file    = DEVICE_PATH_DEFAULT,
+        .socket_address = HOSTNAME_DEFAULT,
+        .socket_port    = PORT_DEFAULT,
+        .tabrmd_bus_type = TCTI_TABRMD_DBUS_TYPE_DEFAULT,
+        .tabrmd_bus_name = TCTI_TABRMD_DBUS_NAME_DEFAULT,
+        .tcti_retries    = TCTI_RETRIES_DEFAULT,
+    };
+
+    get_test_opts_from_env (&opts);
+    if (sanity_check_test_opts (&opts) != 0)
+        exit (1);
+
+    sapi_context0 = sapi_init_from_opts (&opts);
+    if (sapi_context0 == NULL) {
+        g_error ("Failed to create SAPI context.");
+    }
+    /* create an auth session */
+    rc = start_auth_session (sapi_context0, &session_handle);
+    if (rc != TSS2_RC_SUCCESS) {
+        g_error ("Tss2_Sys_StartAuthSession failed: 0x%" PRIxHANDLE, rc);
+    }
+    /* save context */
+    rc = Tss2_Sys_ContextSave (sapi_context0, session_handle, &context);
+    if (rc != TSS2_RC_SUCCESS) {
+        g_error ("Tss2_Sys_ContextSave failed: 0x%" PRIxHANDLE, rc);
+    }
+
+    sapi_context1 = sapi_init_from_opts (&opts);
+    if (sapi_context1 == NULL) {
+        g_error ("Failed to create SAPI context.");
+    }
+    /* reload the session through new connection */
+    rc = Tss2_Sys_ContextLoad (sapi_context1, &context, &session_handle_load);
+    if (rc != TSS2_RC_SUCCESS) {
+        g_error ("Tss2_Sys_ContextLoad failed: 0x%" PRIxHANDLE, rc);
+    }
+    if (session_handle_load == session_handle) {
+        g_info ("session_handle == session_handle_load");
+    } else {
+        g_error ("session_handle != session_handle_load");
+    }
+
+    /* teardown sapi connection */
+    sapi_teardown_full (sapi_context0);
+    sapi_teardown_full (sapi_context1);
+
+    return 0;
+}
