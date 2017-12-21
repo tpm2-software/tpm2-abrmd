058db305f7fe19ad9aa8e5427c6e15ca0b9a33b9
Philip Tricca <philip.b.tricca@intel.com>
2017-12-21 12:35:16 -0800

    test: Rename test setup scripts for clarity.
    int-log-compiler was never a good name. I was trying to keep as close to
    the name of the mechanism (log compiler script) as possible but it's
    misleading. Now that we have two of them making this more clear is
    important.
    
    Signed-off-by: Philip Tricca <philip.b.tricca@intel.com>

--- scripts/int-simulator-setup.sh
+++ scripts/int-simulator-setup.sh
@@ -48,15 +48,18 @@ print_usage ()
 {
     cat <<END
 Usage:
-    run-hwtpm-test.sh --tabrmd-bin=FILE
-                        TEST-SCRIPT [TEST-SCRIPT-ARGUMENTS]
-The '--tabrmd-bin' option is mandatory.
+    int-simulator-setup.sh --simulator-bin=FILE --tabrmd-bin=FILE
+        TEST-SCRIPT [TEST-SCRIPT-ARGUMENTS]
+The '--simulator-bin' and '--tabrmd-bin' options are mandatory.
 END
 }
+SIM_BIN=""
 TABRMD_BIN=""
 while test $# -gt 0; do
     case $1 in
     --help) print_usage; exit $?;;
+    -s|--simulator-bin) SIM_BIN=$2; shift;;
+    -s=*|--simulator-bin=*) SIM_BIN="${1#*=}";;
     -r|--tabrmd-bin) TABRMD_BIN=$2; shift;;
     -r=*|--tabrmd-bin=*) TABRMD_BIN="${1#*=}";;
     --) shift; break;;
@@ -73,8 +76,8 @@ TEST_DIR=$(dirname "$1")
 TEST_NAME=$(basename "${TEST_BIN}")
 
 # sanity tests
-if [ `id -u` != "0" ]; then
-    echo "need the root privilege to launch tabrmd for the integration test"
+if [ ! -x "${SIM_BIN}" ]; then
+    echo "no simulator binary provided or not executable"
     exit 1
 fi
 if [ ! -x "${TABRMD_BIN}" ]; then
@@ -86,18 +89,47 @@ if [ ! -x "${TEST_BIN}" ]; then
     exit 1
 fi
 
-# start an instance of the dbus-daemon for the test, have it use a random port
-DBUS_LOG_FILE=${TEST_BIN}_dbus.log
-DBUS_PID_FILE=${TEST_BIN}_dbus.pid
-echo "Starting dbus-daemon"
-dbus_daemon_start ${DBUS_LOG_FILE} ${DBUS_PID_FILE}
-PID=$(cat ${DBUS_PID_FILE})
-echo "dbus-daemon PID: ${PID}"
+# start an instance of the simulator for the test, have it use a random port
+SIM_LOG_FILE=${TEST_BIN}_simulator.log
+SIM_PID_FILE=${TEST_BIN}_simulator.pid
+SIM_TMP_DIR=$(mktemp --directory --tmpdir=/tmp tpm_server_XXXXXX)
+PORT_MIN=1024
+PORT_MAX=65534
+BACKOFF_FACTOR=2
+BACKOFF=1
+for i in $(seq 10); do
+    SIM_PORT_DATA=$(od -A n -N 2 -t u2 /dev/urandom | awk -v min=${PORT_MIN} -v max=${PORT_MAX} '{print ($1 % (max - min)) + min}')
+    SIM_PORT_CMD=$((${SIM_PORT_DATA}+1))
+    echo "Starting simulator on port ${SIM_PORT_DATA}"
+    simulator_start ${SIM_BIN} ${SIM_PORT_DATA} ${SIM_LOG_FILE} ${SIM_PID_FILE} ${SIM_TMP_DIR}
+    sleep 1 # give daemon time to bind to ports
+    PID=$(cat ${SIM_PID_FILE})
+    echo "simulator PID: ${PID}";
+    netstat -ltpn 2> /dev/null | grep "${PID}" | grep -q "${SIM_PORT_DATA}"
+    ret_data=$?
+    netstat -ltpn 2> /dev/null | grep "${PID}" | grep -q "${SIM_PORT_CMD}"
+    ret_cmd=$?
+    if [ \( $ret_data -eq 0 \) -a \( $ret_cmd -eq 0 \) ]; then
+        echo "Simulator with PID ${PID} bound to port ${SIM_PORT_DATA} and " \
+             "${SIM_PORT_CMD} successfully.";
+        break
+    fi
+    echo "Port conflict? Cleaning up PID: ${PID}"
+    kill "${PID}"
+    BACKOFF=$((${BACKOFF}*${BACKOFF_FACTOR}))
+    echo "Failed to start simulator: port ${SIM_PORT_DATA} or " \
+         "${SIM_PORT_CMD} probably in use. Retrying in ${BACKOFF}."
+    sleep ${BACKOFF}
+    if [ $i -eq 10 ]; then
+        echo "Failed to start simulator after $i tries. Giving up.";
+        exit 1
+    fi
+done
 # start an instance of the tpm2-abrmd daemon for the test, use port from above
 TABRMD_LOG_FILE=${TEST_BIN}_tabrmd.log
 TABRMD_PID_FILE=${TEST_BIN}_tabrmd.pid
-TABRMD_NAME=com.intel.tss2.Tabrmd${PID}
-tabrmd_start ${TABRMD_BIN} ${TABRMD_NAME} ${TABRMD_LOG_FILE} ${TABRMD_PID_FILE}
+TABRMD_NAME=com.intel.tss2.Tabrmd${SIM_PORT_DATA}
+tabrmd_start ${TABRMD_BIN} ${SIM_PORT_DATA} ${TABRMD_NAME} ${TABRMD_LOG_FILE} ${TABRMD_PID_FILE}
 if [ $? -ne 0 ]; then
     echo "failed to start tabrmd with name ${TABRMD_NAME}"
 fi
@@ -106,15 +138,19 @@ fi
 env G_MESSAGES_DEBUG=all TABRMD_TEST_BUS_TYPE=session TABRMD_TEST_BUS_NAME="${TABRMD_NAME}" TABRMD_TEST_TCTI_RETRIES=10 $@
 ret_test=$?
 
+# This sleep is sadly necessary: If we kill the tabrmd w/o sleeping for a
+# second after the test finishes the simulator will die too. Bug in the
+# simulator?
+sleep 1
+
 # teardown tabrmd
 daemon_stop ${TABRMD_PID_FILE}
 ret_tabrmd=$?
 rm -rf ${TABRMD_PID_FILE}
 
-# teardown dbus-daemon
-daemon_stop ${DBUS_PID_FILE}
-ret_dbus=$?
-rm -rf ${DBUS_PID_FILE}
+# teardown simulator, ignore exit code (it's 143?)
+daemon_stop ${SIM_PID_FILE}
+rm -rf ${SIM_TMP_DIR} ${SIM_PID_FILE}
 
 # handle exit codes
 if [ $ret_test -ne 0 ]; then
@@ -124,10 +160,6 @@ fi
 if [ $ret_tabrmd -ne 0 ]; then
     echo "Execution of tabrmd failed: $ret_tabrmd"
     exit $ret_tabrmd
-fi
-if [ $ret_dbus -ne 0 ]; then
-    echo "Execution of dbus-daemon failed: $ret_dbus"
-    exit $ret_dbus
 fi
 
 exit 0
