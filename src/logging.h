fc93abcd33517a62c223abbfe31cbd6186f4a3b6
Philip Tricca <philip.b.tricca@intel.com>
2018-01-12 21:46:36 -0800

    test: Add unit test for logging module.
    Signed-off-by: Philip Tricca <philip.b.tricca@intel.com>

--- src/logging.h
+++ src/logging.h
@@ -33,4 +33,15 @@
         exit (EXIT_FAILURE); \
     } while (0)
 
+#define LOG_LEVEL_DEFAULT (G_LOG_LEVEL_ERROR | G_LOG_LEVEL_CRITICAL | \
+                           G_LOG_LEVEL_WARNING)
+#define LOG_LEVEL_ALL     (LOG_LEVEL_DEFAULT | G_LOG_LEVEL_MESSAGE | \
+                           G_LOG_LEVEL_INFO | G_LOG_LEVEL_DEBUG)
+
+void
+syslog_log_handler (const char     *log_domain,
+                    GLogLevelFlags  log_level,
+                    const char     *message,
+                    gpointer        log_config_list);
+int get_enabled_log_levels (void);
 gint set_logger (gchar *name);
