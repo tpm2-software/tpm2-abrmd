tpm2-software/tpm2-abrmd

commit 2f41afb0c890894ddc4cca72d04a1df28805e907

tcti: add spport for dynamic init interface
Add the dynamic tcti interface, and export the
module information and symbols.

Fixes: #277

Signed-off-by: William Roberts <william.c.roberts@intel.com>

src/tcti-tabrmd.c
@@ -522,3 +522,26 @@ tss2_tcti_tabrmd_init (TSS2_TCTI_CONTEXT *context,
                                        TCTI_TABRMD_DBUS_TYPE_DEFAULT,
                                        TCTI_TABRMD_DBUS_NAME_DEFAULT);
 }
+
+static TSS2_RC
+tss2_tcti_tabrmd_init_config (TSS2_TCTI_CONTEXT *context,
+                              size_t            *size,
+                              const char        *conf)
+{
+	(void)conf;
+	return tss2_tcti_tabrmd_init(context, size);
+}
+
+/* public info structure */
+const static TSS2_TCTI_INFO tss2_tcti_info = {
+    .name = "tcti-abrmd",
+    .description = "TCTI module for communication with tabrmd.",
+    .config_help = "This module takes NO arguments.",
+    .init = tss2_tcti_tabrmd_init_config,
+};
+
+const TSS2_TCTI_INFO*
+Tss2_Tcti_Info (void)
+{
+    return &tss2_tcti_info;
+}

src/tcti-tabrmd.map
@@ -3,6 +3,7 @@
         tss2_tcti_tabrmd_init;
         tss2_tcti_tabrmd_init_full;
         tss2_tcti_tabrmd_dump_trans_state;
+        Tss2_Tcti_Info;
     local:
         *;
 };

commit f5d0d21ac0f3f4b0fbbc21c907c7537d5ea998e9

tcti-tabrmd: Fix whitespace errors introduced in 2f41afb0.
Per CONTRIBUTING.md and the doc/coding_standard_c.md we indent with
spaces, in multiples of 4, not tabs.

Signed-off-by: Philip Tricca <philip.b.tricca@intel.com>

src/tcti-tabrmd.c
@@ -528,8 +528,8 @@ tss2_tcti_tabrmd_init_config (TSS2_TCTI_CONTEXT *context,
                               size_t            *size,
                               const char        *conf)
 {
-	(void)conf;
-	return tss2_tcti_tabrmd_init(context, size);
+    (void)conf;
+    return tss2_tcti_tabrmd_init(context, size);
 }
 
 /* public info structure */