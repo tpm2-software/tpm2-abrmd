377792a84bd88cecff695dd0b08a9fdf45ba9b2c
Philip Tricca <philip.b.tricca@intel.com>
2018-02-14 16:14:57 -0800

    tcti-util: Allow shortened TCTI library names to align with the tools.
    This should now make the --tcti option to the tabrmd identical to the
    same one in the tools. This means that the function that dlopens the
    TCTI library will try to prepend 'libtcti-' and append '.so' to the
    provided name if the first attempt to load the library fails.
    
    Signed-off-by: Philip Tricca <philip.b.tricca@intel.com>

--- src/tcti-util.c
+++ src/tcti-util.c
@@ -27,6 +27,7 @@
 #include <dlfcn.h>
 #include <glib.h>
 #include <inttypes.h>
+#include <stdio.h>
 
 #include "tcti-util.h"
 #include "tabrmd.h"
@@ -44,12 +45,27 @@ tcti_util_discover_info (const char *filename,
                          void **tcti_dl_handle)
 {
     TSS2_TCTI_INFO_FUNC info_func;
+    gchar filename_xfrm [PATH_MAX];
+    size_t size;
 
     g_debug ("%s", __func__);
     *tcti_dl_handle = dlopen (filename, RTLD_LAZY);
     if (*tcti_dl_handle == NULL) {
-        g_warning ("failed to dlopen file %s: %s", filename, dlerror ());
-        return TSS2_RESMGR_RC_BAD_VALUE;
+        size = snprintf (filename_xfrm,
+                         sizeof (filename_xfrm),
+                         "libtcti-%s.so",
+                         filename);
+        if (size >= sizeof (filename_xfrm)) {
+            g_critical ("TCTI name truncated in transform.");
+            return TSS2_RESMGR_RC_BAD_VALUE;
+        }
+        g_debug ("dlopen failed on \"%s\", trying \"%s\"",
+                 filename, filename_xfrm);
+        *tcti_dl_handle = dlopen (filename_xfrm, RTLD_LAZY);
+        if (*tcti_dl_handle == NULL) {
+            g_warning ("failed to dlopen library: %s", filename);
+            return TSS2_RESMGR_RC_BAD_VALUE;
+        }
     }
     info_func = dlsym (*tcti_dl_handle, TSS2_TCTI_INFO_SYMBOL);
     if (info_func == NULL) {
