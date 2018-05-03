ad5bb2565d006447896c287815a8f7bb38a43fae
Philip Tricca <philip.b.tricca@intel.com>
2018-05-03 12:02:49 -0700

    ResourceManager: Add new session to list of loaded sessions when mapping.
    The logic in the 'create_context_mapping_session' function was causing
    us to run out of space for loaded sessions. This in turn caused the
    extended session logic to break down since those tests load and unload
    a lot of sessions.
    
    The problem was the logic that detected which of the internal data
    structures held a SessionEntry after a new session was created. The
    ResourceManager wasn't adding it to the list of loaded sessions and so
    the ResourceManager wouldn't unload them. This caused us to run out of
    space for loaded sessions very quickly.
    
    This commit also adds a comment block above the SessionEntry state
    enumeration to make it clear what the states mean and the constraints
    that apply when a session is in a given state.
    
    Finally this commit also moves the test/integration/auth-session-max.int
    test back to the group of expected failures (XFAIL). The bug fixed above
    is what caused this test to pass and now that it's fixed this test to
    fail again.
    
    Signed-off-by: Philip Tricca <philip.b.tricca@intel.com>

--- src/session-entry-state-enum.h
+++ src/session-entry-state-enum.h
@@ -31,6 +31,26 @@
 
 G_BEGIN_DECLS
 
+/*
+ * These define the possible states that a SessionEntry object may be in. The
+ * states are defined by some previous action performed over the connection
+ * that created the session / SessionEntry object.
+ * - SESSION_ENTRY_SAVED_RM: All SessionEntry objects are in this state when
+ *   they are first created. SessionEntry objects are created when the
+ *   StartAuthSession command comes in over a connection. When the response
+ *   comes back from the TPM the ResourceManager extracts the handle and saves
+ *   the session.
+ * - SESSION_ENTRY_SAVED_CLIENT: A SessionEntry transitions to this state when
+ *   the client saves the session context by way of the TPM_CC_ContextSave
+ *   command. Once a SessionEntry is in this state it must be explicitly
+ *   loaded by a client before it can be used again.
+ * - SESSION_ETNRY_SAVED_CLIENT_CLOSED: A SessionEntry transitions to this
+ *   state from the SESSION_ENTRY_SAVED_CLIENT state when the connection used
+ *   to create and save the session is closed. A session previously saved by
+ *   the client will not be flushed by the ResourceManager which allows other
+ *   connections to claim the session but only if they're in posession of the
+ *   context blob.
+ */
 typedef enum SESSION_ENTRY_STATE {
     SESSION_ENTRY_SAVED_RM,
     SESSION_ENTRY_SAVED_CLIENT,
