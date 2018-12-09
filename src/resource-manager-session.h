1c3519713879b68b76075163c1376da1d8deceed
Philip Tricca <philip.b.tricca@intel.com>
2018-12-08 20:48:01 -0800

    ResourceManager: Refactor session management functions.
    The functions required to manage session context blobs are fairly self
    contained. Separating them out into a separate code module called
    'resource-manager-session' reduces the clutter in the main
    resource-manager module. Similar refactoring for transient object
    management would be nice.
    
    Signed-off-by: Philip Tricca <philip.b.tricca@intel.com>

---
+++ src/resource-manager-session.h
@@ -0,0 +1,22 @@
+/* SPDX-License-Identifier: BSD-2 */
+#ifndef RESOURCE_MANAGER_SESSION_H
+#define RESOURCE_MANAGER_SESSION_H
+
+#include <glib.h>
+#include <inttypes.h>
+
+#include <tss2/tss2_tpm2_types.h>
+
+#include "resource-manager.h"
+#include "session-entry.h"
+
+Tpm2Response*
+load_session (ResourceManager *resmgr,
+              SessionEntry *entry);
+gboolean
+flush_session (ResourceManager *resmgr,
+               SessionEntry *entry);
+Tpm2Response*
+save_session (ResourceManager *resmgr,
+              SessionEntry *entry);
+#endif
