tpm2-software/tpm2-abrmd

commit c5e66b78def9a7f6f6bf47d778a2795af306f73c

test: Add unit tests for tabrmd-options module.
These unit tests are pretty straight forward. The most complicated part
is wrapping the `g_option_context_add_main_entries` function such that
we can selectively set values in the tabrmd_options_t structure using
the GOptionEntry array.

As part of adding these tests this commit also fixes two bugs in the
`tcti_conf_parse` function. A null or empty string passed in the
`combined_conf` string should cause the function to exit returning
FALSE.

Signed-off-by: Philip Tricca <philip.b.tricca@intel.com>

src/tabrmd-options.c
@@ -71,11 +71,11 @@ tcti_conf_parse (gchar *combined_conf,
     }
     if (combined_conf == NULL) {
         g_debug ("%s: combined conf is null", __func__);
-        return TRUE;
+        return FALSE;
     }
     if (strlen (combined_conf) == 0) {
         g_debug ("%s: combined conf is the empty string", __func__);
-        return TRUE;
+        return FALSE;
     }
 
     split = strchr (combined_conf, ':');

test/tabrmd-options_unit.c
@@ -1,0 +1,265 @@
+/* SPDX-License-Identifier: BSD-2 */
+/*
+ * Copyright (c) 2017 - 2018, Intel Corporation
+ * All rights reserved.
+ */
+#include <glib.h>
+#include <inttypes.h>
+#include <string.h>
+#include <unistd.h>
+
+#include <setjmp.h>
+#include <cmocka.h>
+
+#include "tabrmd-options.h"
+#include "util.h"
+
+static void
+tcti_conf_parse_test_null_tcti (void **state)
+{
+    UNUSED_PARAM (state);
+    assert_false (tcti_conf_parse (NULL, NULL, NULL));
+}
+static void
+tcti_conf_parse_test_null_conf (void **state)
+{
+    UNUSED_PARAM (state);
+    gchar *tcti_filename, *tcti_conf;
+
+    assert_false (tcti_conf_parse (NULL, &tcti_filename, &tcti_conf));
+}
+static void
+tcti_conf_parse_test_zero_conf (void **state)
+{
+    UNUSED_PARAM (state);
+    gchar *tcti_filename, *tcti_conf;
+
+    assert_false (tcti_conf_parse ("", &tcti_filename, &tcti_conf));
+}
+static void
+tcti_conf_parse_test_name_only (void **state)
+{
+    UNUSED_PARAM (state);
+    gchar *combined_conf = "tcti_name", *tcti_filename, *tcti_conf;
+
+    assert_true (tcti_conf_parse (combined_conf, &tcti_filename, &tcti_conf));
+    assert_string_equal (combined_conf, tcti_filename);
+}
+static void
+tcti_conf_parse_test_name_conf (void **state)
+{
+    UNUSED_PARAM (state);
+    gchar combined_conf[] = "tcti_name:tcti_conf", *tcti_filename, *tcti_conf;
+
+    assert_true (tcti_conf_parse (combined_conf, &tcti_filename, &tcti_conf));
+}
+GOptionContext*
+__wrap_g_option_context_new (const gchar *parameter_string)
+{
+    UNUSED_PARAM (parameter_string);
+
+    g_warning ("%s", __func__);
+    return mock_type (GOptionContext*);
+}
+void
+__wrap_g_option_context_add_main_entries (
+    GOptionContext *context,
+    const GOptionEntry *entries,
+    const gchar *translation_domain)
+{
+    UNUSED_PARAM (context);
+    UNUSED_PARAM (entries);
+    UNUSED_PARAM (translation_domain);
+
+    char *long_name = mock_type (char*);
+    size_t i;
+
+    for (i = 0; entries [i].long_name != NULL; ++i) {
+        if (strcmp (long_name, entries [i].long_name) == 0) {
+            if (strcmp (long_name, "max-connections") == 0 ||
+                strcmp (long_name, "max-sessions") == 0 ||
+                strcmp (long_name, "max-transients") == 0)
+            {
+                *(guint*)entries [i].arg_data = mock_type (guint);
+            }
+            if (strcmp (long_name, "tcti") == 0) {
+                *(char**)entries [i].arg_data = mock_type (char*);
+            }
+        }
+    }
+
+    g_warning ("%s", __func__);
+    return;
+}
+
+gboolean
+__wrap_g_option_context_parse (GOptionContext *context,
+                               gint *argc,
+                               gchar ***argv,
+                               GError **error)
+{
+    UNUSED_PARAM (context);
+    UNUSED_PARAM (argc);
+    UNUSED_PARAM (argv);
+
+    g_warning ("%s", __func__);
+    *error = mock_type (GError*);
+    return mock_type (gboolean);
+}
+static void
+tcti_conf_parse_opts_parse_fail (void **state)
+{
+    UNUSED_PARAM (state);
+    tabrmd_options_t options = TABRMD_OPTIONS_INIT_DEFAULT;
+    GOptionContext *ctx = NULL;
+    int argc = 0;
+    char **argv = NULL;
+    GError error = { .message = "foo", };
+
+    will_return (__wrap_g_option_context_new, ctx);
+    will_return (__wrap_g_option_context_add_main_entries, "foo");
+    will_return (__wrap_g_option_context_parse, &error);
+    will_return (__wrap_g_option_context_parse, FALSE);
+    assert_false (parse_opts (argc, argv, &options));
+}
+gint
+__wrap_set_logger (gchar *name)
+{
+    UNUSED_PARAM (name);
+    return mock_type (gint);
+}
+static void
+tcti_conf_parse_opts_logger_fail (void **state)
+{
+    UNUSED_PARAM (state);
+    tabrmd_options_t options = TABRMD_OPTIONS_INIT_DEFAULT;
+    GOptionContext *ctx = NULL;
+    int argc = 0;
+    char **argv = NULL;
+    GError error = { .message = "foo", };
+
+    will_return (__wrap_g_option_context_new, ctx);
+    will_return (__wrap_g_option_context_add_main_entries, "foo");
+    will_return (__wrap_g_option_context_parse, &error);
+    will_return (__wrap_g_option_context_parse, TRUE);
+    will_return (__wrap_set_logger, -1);
+    assert_false (parse_opts (argc, argv, &options));
+}
+static void
+tcti_conf_parse_opts_max_connections_fail (void **state)
+{
+    UNUSED_PARAM (state);
+    tabrmd_options_t options = TABRMD_OPTIONS_INIT_DEFAULT;
+    GOptionContext *ctx = NULL;
+    int argc = 0;
+    char **argv = NULL;
+    GError error = { .message = "foo", };
+
+    will_return (__wrap_g_option_context_new, ctx);
+    will_return (__wrap_g_option_context_add_main_entries, "max-connections");
+    will_return (__wrap_g_option_context_add_main_entries, 0);
+    will_return (__wrap_g_option_context_parse, &error);
+    will_return (__wrap_g_option_context_parse, TRUE);
+    will_return (__wrap_set_logger, 0);
+    assert_false (parse_opts (argc, argv, &options));
+}
+static void
+tcti_conf_parse_opts_max_sessions_fail (void **state)
+{
+    UNUSED_PARAM (state);
+    tabrmd_options_t options = TABRMD_OPTIONS_INIT_DEFAULT;
+    GOptionContext *ctx = NULL;
+    int argc = 0;
+    char **argv = NULL;
+    GError error = { .message = "foo", };
+
+    will_return (__wrap_g_option_context_new, ctx);
+    will_return (__wrap_g_option_context_add_main_entries, "max-sessions");
+    will_return (__wrap_g_option_context_add_main_entries, 0);
+    will_return (__wrap_g_option_context_parse, &error);
+    will_return (__wrap_g_option_context_parse, TRUE);
+    will_return (__wrap_set_logger, 0);
+    assert_false (parse_opts (argc, argv, &options));
+}
+static void
+tcti_conf_parse_opts_max_transient_fail (void **state)
+{
+    UNUSED_PARAM (state);
+    tabrmd_options_t options = TABRMD_OPTIONS_INIT_DEFAULT;
+    GOptionContext *ctx = NULL;
+    int argc = 0;
+    char **argv = NULL;
+    GError error = { .message = "foo", };
+
+    will_return (__wrap_g_option_context_new, ctx);
+    will_return (__wrap_g_option_context_add_main_entries, "max-transients");
+    will_return (__wrap_g_option_context_add_main_entries, 0);
+    will_return (__wrap_g_option_context_parse, &error);
+    will_return (__wrap_g_option_context_parse, TRUE);
+    will_return (__wrap_set_logger, 0);
+    assert_false (parse_opts (argc, argv, &options));
+}
+static void
+tcti_conf_parse_opts_tcti_conf_fail (void **state)
+{
+    UNUSED_PARAM (state);
+    tabrmd_options_t options = TABRMD_OPTIONS_INIT_DEFAULT;
+    GOptionContext *ctx = NULL;
+    int argc = 0;
+    char **argv = NULL;
+    GError error = { .message = "foo", };
+
+    options.tcti_filename = NULL;
+    will_return (__wrap_g_option_context_new, ctx);
+    will_return (__wrap_g_option_context_add_main_entries, "foo");
+    will_return (__wrap_g_option_context_parse, &error);
+    will_return (__wrap_g_option_context_parse, TRUE);
+    will_return (__wrap_set_logger, 0);
+    assert_false (parse_opts (argc, argv, &options));
+}
+void
+__wrap_g_option_context_free (GOptionContext *context)
+{
+    UNUSED_PARAM (context);
+
+    g_warning ("%s", __func__);
+    return;
+}
+static void
+tcti_conf_parse_opts_success (void **state)
+{
+    UNUSED_PARAM (state);
+    tabrmd_options_t options = TABRMD_OPTIONS_INIT_DEFAULT;
+    GOptionContext *ctx = NULL;
+    int argc = 0;
+    char **argv = NULL;
+    GError error = { .message = "foo", };
+
+    will_return (__wrap_g_option_context_new, ctx);
+    will_return (__wrap_g_option_context_add_main_entries, "tcti");
+    will_return (__wrap_g_option_context_add_main_entries, "foo");
+    will_return (__wrap_g_option_context_parse, &error);
+    will_return (__wrap_g_option_context_parse, TRUE);
+    will_return (__wrap_set_logger, 0);
+    assert_true (parse_opts (argc, argv, &options));
+}
+
+int
+main (void)
+{
+    const struct CMUnitTest tests[] = {
+        cmocka_unit_test (tcti_conf_parse_test_null_tcti),
+        cmocka_unit_test (tcti_conf_parse_test_null_conf),
+        cmocka_unit_test (tcti_conf_parse_test_zero_conf),
+        cmocka_unit_test (tcti_conf_parse_test_name_only),
+        cmocka_unit_test (tcti_conf_parse_test_name_conf),
+        cmocka_unit_test (tcti_conf_parse_opts_parse_fail),
+        cmocka_unit_test (tcti_conf_parse_opts_logger_fail),
+        cmocka_unit_test (tcti_conf_parse_opts_max_connections_fail),
+        cmocka_unit_test (tcti_conf_parse_opts_max_sessions_fail),
+        cmocka_unit_test (tcti_conf_parse_opts_max_transient_fail),
+        cmocka_unit_test (tcti_conf_parse_opts_tcti_conf_fail),
+        cmocka_unit_test (tcti_conf_parse_opts_success),
+    };
+    return cmocka_run_group_tests (tests, NULL, NULL);
+}

Makefile.am
@@ -33,6 +33,7 @@ TESTS_UNIT = \
     test/random_unit \
     test/session-entry_unit \
     test/session-list_unit \
+    test/tabrmd-options_unit \
     test/test-skeleton_unit \
     test/tcti_unit \
     test/tcti-factory_unit \
@@ -314,6 +315,13 @@ define make_parent_dir
 endef
 
 if UNIT
+test_tabrmd_options_unit_CFLAGS = $(UNIT_AM_CFLAGS) $(GLIB_CFLAGS)
+test_tabrmd_options_unit_LDADD = $(CMOCKA_LIBS) $(GLIB_LIBS) $(libutil)
+test_tabrmd_options_unit_LDFLAGS = -Wl,--wrap=g_option_context_new \
+    -Wl,--wrap=g_option_context_add_main_entries,--wrap=g_option_context_parse \
+    -Wl,--wrap=set_logger,--wrap=g_option_context_free
+test_tabrmd_options_unit_SOURCES = test/tabrmd-options_unit.c
+
 test_test_skeleton_unit_CFLAGS  = $(UNIT_AM_CFLAGS)
 test_test_skeleton_unit_LDADD   = $(CMOCKA_LIBS)
 test_test_skeleton_unit_SOURCES = test/test-skeleton_unit.c

src/tabrmd-options.h
@@ -39,5 +39,9 @@ gboolean
 parse_opts (gint argc,
             gchar *argv[],
             tabrmd_options_t *options);
+gboolean
+tcti_conf_parse (gchar *combined_conf,
+                 gchar **tcti_filename,
+                 gchar **tcti_conf);
 
 #endif /* TABRMD_OPTIONS_H */

src/tabrmd.h
@@ -6,7 +6,7 @@
 #ifndef TSS2_TABD_H
 #define TSS2_TABD_H
 
-#include <glib.h>
+#include <gio/gio.h>
 
 #include <tss2/tss2_tpm2_types.h>
 #include <tss2/tss2_tcti.h>

commit fe0cb223a3aef064077681b1aee4678ae3fd1faa

tabrmd-options: Fix bug introduced in c5e66b78de.
The comment above this function clearly states the reason for these
return values. This was however missed in this refactoring. This fix
includes a small change to free the GOptionContext as early as possible.
This allows us to eliminate the final conditional returning the status
from the last function call directly.

This allows us to eliminate one of the unit tests without affecting the
coverage metrics.

Signed-off-by: Philip Tricca <philip.b.tricca@intel.com>

src/tabrmd-options.c
@@ -65,17 +65,17 @@ tcti_conf_parse (gchar *combined_conf,
 
     g_debug ("%s", __func__);
     if (tcti_filename == NULL || tcti_conf == NULL) {
-        g_info ("%s: tcti_filename and tcti_conf out params may not be NULL",
-                __func__);
+        g_critical ("%s: tcti_filename and tcti_conf out params may not be NULL",
+                    __func__);
         return FALSE;
     }
     if (combined_conf == NULL) {
         g_debug ("%s: combined conf is null", __func__);
-        return FALSE;
+        return TRUE;
     }
     if (strlen (combined_conf) == 0) {
         g_debug ("%s: combined conf is the empty string", __func__);
-        return FALSE;
+        return TRUE;
     }
 
     split = strchr (combined_conf, ':');
@@ -160,6 +160,7 @@ parse_opts (gint argc,
         g_critical ("Failed to parse options: %s", err->message);
         return FALSE;
     }
+    g_option_context_free (ctx);
     /* select the bus type, default to G_BUS_TYPE_SESSION */
     options->bus = session_bus ? G_BUS_TYPE_SESSION : G_BUS_TYPE_SYSTEM;
     if (set_logger (logger_name) == -1) {
@@ -187,12 +188,7 @@ parse_opts (gint argc,
                     TABRMD_TRANSIENT_MAX);
         return FALSE;
     }
-    if (!tcti_conf_parse (tcti_optconf,
-                          &options->tcti_filename,
-                          &options->tcti_conf)) {
-        g_critical ("bad tcti conf string");
-        return FALSE;
-    }
-    g_option_context_free (ctx);
-    return TRUE;
+    return tcti_conf_parse (tcti_optconf,
+                            &options->tcti_filename,
+                            &options->tcti_conf);
 }

test/tabrmd-options_unit.c
@@ -26,7 +26,7 @@ tcti_conf_parse_test_null_conf (void **state)
     UNUSED_PARAM (state);
     gchar *tcti_filename, *tcti_conf;
 
-    assert_false (tcti_conf_parse (NULL, &tcti_filename, &tcti_conf));
+    assert_true (tcti_conf_parse (NULL, &tcti_filename, &tcti_conf));
 }
 static void
 tcti_conf_parse_test_zero_conf (void **state)
@@ -34,7 +34,7 @@ tcti_conf_parse_test_zero_conf (void **state)
     UNUSED_PARAM (state);
     gchar *tcti_filename, *tcti_conf;
 
-    assert_false (tcti_conf_parse ("", &tcti_filename, &tcti_conf));
+    assert_true (tcti_conf_parse ("", &tcti_filename, &tcti_conf));
 }
 static void
 tcti_conf_parse_test_name_only (void **state)
@@ -199,24 +199,6 @@ tcti_conf_parse_opts_max_transient_fail (void **state)
     will_return (__wrap_set_logger, 0);
     assert_false (parse_opts (argc, argv, &options));
 }
-static void
-tcti_conf_parse_opts_tcti_conf_fail (void **state)
-{
-    UNUSED_PARAM (state);
-    tabrmd_options_t options = TABRMD_OPTIONS_INIT_DEFAULT;
-    GOptionContext *ctx = NULL;
-    int argc = 0;
-    char **argv = NULL;
-    GError error = { .message = "foo", };
-
-    options.tcti_filename = NULL;
-    will_return (__wrap_g_option_context_new, ctx);
-    will_return (__wrap_g_option_context_add_main_entries, "foo");
-    will_return (__wrap_g_option_context_parse, &error);
-    will_return (__wrap_g_option_context_parse, TRUE);
-    will_return (__wrap_set_logger, 0);
-    assert_false (parse_opts (argc, argv, &options));
-}
 void
 __wrap_g_option_context_free (GOptionContext *context)
 {
@@ -258,7 +240,6 @@ main (void)
         cmocka_unit_test (tcti_conf_parse_opts_max_connections_fail),
         cmocka_unit_test (tcti_conf_parse_opts_max_sessions_fail),
         cmocka_unit_test (tcti_conf_parse_opts_max_transient_fail),
-        cmocka_unit_test (tcti_conf_parse_opts_tcti_conf_fail),
         cmocka_unit_test (tcti_conf_parse_opts_success),
     };
     return cmocka_run_group_tests (tests, NULL, NULL);