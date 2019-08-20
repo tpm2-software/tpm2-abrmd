tpm2-software/tpm2-abrmd

commit 665ec3b6a1fed484d58154dad257705f28524f39

CommandSource: Move TPM command buffer read / alloc function to util.
The util module already had most of this functionality. The code in
the CommandSource that allocates the buffer to hold the command buffer
was the only unique code. This code layered quite nicely on top of the
existing read_tpm_buffer function.

This patch also fixes up the tests in the CommandSource unit test.
It also adds unit tests for the new functions to the util unit test.
Finally we've added a few debug / warning messages that were helpful
in debugging this new functionality.

Signed-off-by: Philip Tricca <philip.b.tricca@intel.com>

src/util.c
@@ -225,6 +225,57 @@ read_tpm_buffer (GSocket                  *socket,
     /* Now that we have the header, we know the whole buffer size. Get it. */
     return read_data (socket, index, buf, size - *index);
 }
+/*
+ * This fucntion is a wrapper around the read_tpm_buffer function above. It
+ * adds the memory allocation logic necessary to create the buffer to hold
+ * the TPM command / response buffer.
+ * Returns NULL on error, and a pointer to the allocated buffer on success.
+ *   The size of the allocated buffer is returned through the *buf_size
+ *   parameter on success.
+ */
+uint8_t*
+read_tpm_buffer_alloc (GSocket  *socket,
+                       size_t   *buf_size)
+{
+    uint8_t *buf = NULL;
+    size_t   size_tmp = TPM_HEADER_SIZE, index = 0;
+    int ret = 0;
+
+    if (socket == NULL || buf_size == NULL) {
+        g_warning ("%s: got null parameter", __func__);
+        return NULL;
+    }
+    do {
+        buf = g_realloc (buf, size_tmp);
+        ret = read_tpm_buffer (socket, &index, buf, size_tmp);
+        switch (ret) {
+        case EPROTO:
+            size_tmp = get_command_size (buf);
+            if (size_tmp < TPM_HEADER_SIZE && size_tmp > UTIL_BUF_MAX) {
+                g_warning ("%s: tpm buffer size is ouside of acceptable bounds: %zd",
+                           __func__, size_tmp);
+                goto err_out;
+            }
+            break;
+        case 0:
+            /* done */
+            break;
+        default:
+            goto err_out;
+        }
+    } while (ret == EPROTO);
+    g_debug ("%s: read TPM buffer to 0x%" PRIxPTR " of size: %zd",
+             __func__, (uintptr_t)buf, index);
+    g_debug_bytes (buf, index, 16, 4);
+    *buf_size = size_tmp;
+    return buf;
+err_out:
+    g_debug ("%s: err_out freeing buffer at 0x%" PRIxPTR, __func__, (uintptr_t)buf);
+    if (buf != NULL) {
+        g_free (buf);
+    }
+    return NULL;
+}
 /* pretty print */
 void
 g_debug_tpma_cc (TPMA_CC tpma_cc)

Makefile.am
@@ -327,7 +327,7 @@ test_command_attrs_unit_SOURCES  = test/command-attrs_unit.c
 
 test_command_source_unit_CFLAGS  = $(UNIT_AM_CFLAGS)
 test_command_source_unit_LDADD   = $(CMOCKA_LIBS) $(GLIB_LIBS) $(SAPI_LIBS) $(PTHREAD_LIBS) $(GOBJECT_LIBS) $(libutil)
-test_command_source_unit_LDFLAGS = -Wl,--wrap=connection_manager_lookup_fd,--wrap=sink_enqueue,--wrap=read_data
+test_command_source_unit_LDFLAGS = -Wl,--wrap=connection_manager_lookup_fd,--wrap=sink_enqueue,--wrap=read_tpm_buffer_alloc
 test_command_source_unit_SOURCES = test/command-source_unit.c
 
 test_handle_map_entry_unit_CFLAGS  = $(UNIT_AM_CFLAGS)

src/command-source.c
@@ -289,9 +289,7 @@ process_client_fd (CommandSource      *source,
     Tpm2Command *command;
     Connection  *connection;
     uint8_t *buf = NULL;
-    uint32_t command_size = 0;
-    size_t index = 0;
-    int ret = 0;
+    size_t command_size = 0;
     GSocket *socket;
 
     g_debug ("process_client_fd 0x%" PRIxPTR, (uintptr_t)source);
@@ -302,30 +300,13 @@ process_client_fd (CommandSource      *source,
         g_debug ("connection_manager_lookup_fd for fd %d: 0x%" PRIxPTR, fd,
                  (uintptr_t)connection);
     socket = connection_get_gsocket (connection);
-    buf = g_malloc0 (TPM_HEADER_SIZE);
-    ret = read_data (socket, &index, buf, TPM_HEADER_SIZE);
-    if (ret == 0) {
-        g_debug_bytes (buf, index, 16, 4);
-    } else {
-        goto fail_out;
-    }
-    command_size = get_command_size (buf);
-    if (command_size > TPM_HEADER_SIZE && command_size <= UTIL_BUF_MAX) {
-        buf = g_realloc (buf, command_size);
-        ret = read_data (socket, &index, buf, command_size - TPM_HEADER_SIZE);
-        if (ret == 0) {
-            g_debug_bytes (buf, index, 16, 4);
-        } else {
-            goto fail_out;
-        }
-    } else if (command_size == TPM_HEADER_SIZE) {
-        /* No more data. Command has no parameters / handles / auths etc. */
-    } else {
+    buf = read_tpm_buffer_alloc (socket, &command_size);
+    if (buf == NULL) {
         goto fail_out;
     }
     attributes = command_attrs_from_cc (source->command_attrs,
                                         get_command_code (buf));
-    command = tpm2_command_new (connection, buf, index, attributes);
+    command = tpm2_command_new (connection, buf, command_size, attributes);
     if (command != NULL) {
         sink_enqueue (source->sink, G_OBJECT (command));
         /* the sink now owns this message */

src/tpm2-command.c
@@ -478,6 +478,7 @@ tpm2_command_get_flush_handle (Tpm2Command *command,
         return RM_RC (TPM_RC_TYPE);
     }
     if (command->buffer_size < TPM_HEADER_SIZE + sizeof (TPM_HANDLE)) {
+        g_warning ("%s: command buffer_size insufficient", __func__);
         *handle = 0;
         return RM_RC (TPM_RC_INSUFFICIENT);
     }

src/util.h
@@ -65,6 +65,8 @@ int         read_tpm_buffer                 (GSocket          *socket,
                                              size_t           *index,
                                              uint8_t          *buf,
                                              size_t            buf_size);
+uint8_t*    read_tpm_buffer_alloc           (GSocket          *socket,
+                                             size_t           *buf_size);
 void        g_debug_bytes                   (uint8_t const    *byte_array,
                                              size_t            array_size,
                                              size_t            width,

test/command-source_unit.c
@@ -62,15 +62,20 @@ __wrap_connection_manager_lookup_fd (ConnectionManager *manager,
     g_debug ("__wrap_connection_manager_lookup_fd");
     return CONNECTION (mock ());
 }
-int
-__wrap_read_data (int                       fd,
-                  size_t                   *index,
-                  uint8_t                  *buf,
-                  size_t                    count)
+uint8_t*
+__wrap_read_tpm_buffer_alloc (GSocket   *socket,
+                              size_t    *buf_size)
 {
-    memcpy (&buf[*index], mock_type(uint8_t*), count);
-    *index = mock_type (size_t);
-    return mock_type (int);
+    uint8_t *buf_src = mock_type (uint8_t*);
+    uint8_t *buf_dst = NULL;
+    size_t   size = mock_type (size_t);
+
+    g_debug ("%s", __func__);
+    buf_dst = g_malloc0 (size);
+    memcpy (buf_dst, buf_src, size);
+    *buf_size = size;
+
+    return buf_dst;
 }
 void
 __wrap_sink_enqueue (Sink     *sink,
@@ -279,14 +284,9 @@ command_source_process_client_fd_test (void **state)
         /* prime wraps */
     will_return (__wrap_connection_manager_lookup_fd, connection);
 
-    /* setup read of header */
-    will_return (__wrap_read_data, &data_in [0]);
-    will_return (__wrap_read_data, 10);
-    will_return (__wrap_read_data, 0);
-    /* setup read of body */
-    will_return (__wrap_read_data, &data_in [10]);
-    will_return (__wrap_read_data, sizeof (data_in) - 10);
-    will_return (__wrap_read_data, 0);
+    /* setup read of tpm buffer */
+    will_return (__wrap_read_tpm_buffer_alloc, data_in);
+    will_return (__wrap_read_tpm_buffer_alloc, sizeof (data_in));
 
     will_return (__wrap_sink_enqueue, &command_out);
 

test/util_unit.c
@@ -183,6 +183,7 @@ read_data_setup (void **state)
 
     data = calloc (1, sizeof (data_t));
     data->buf_size = 26;
+    data->socket = (GSocket*)1;
 
     *state = data;
     return 0;
@@ -592,6 +593,72 @@ read_tpm_buf_populated_body_test (void **state)
     assert_memory_equal (data->buf_out, buf_in, data->buf_size);
 }
 
+static void
+read_tpm_buf_alloc_success_test (void **state)
+{
+    data_t *data = *state;
+    uint8_t *buf;
+    size_t   buf_size;
+
+    /* prime read to successfully produce the header */
+    will_return (__wrap_g_socket_receive, buf_in);
+    will_return (__wrap_g_socket_receive, 0);
+    will_return (__wrap_g_socket_receive, 0);
+    will_return (__wrap_g_socket_receive, 10);
+    /* prime read to successfully produce the rest of the buffer */
+    will_return (__wrap_g_socket_receive, buf_in);
+    will_return (__wrap_g_socket_receive, 10);
+    will_return (__wrap_g_socket_receive, 0);
+    will_return (__wrap_g_socket_receive, data->buf_size - 10);
+
+    buf = read_tpm_buffer_alloc (data->socket,
+                                 &buf_size);
+    assert_non_null (buf);
+    assert_int_equal (buf_size, data->buf_size);
+    assert_memory_equal (buf, buf_in, data->buf_size);
+    g_free (buf);
+}
+
+static void
+read_tpm_buf_alloc_header_only_test (void **state)
+{
+    data_t *data = *state;
+    uint8_t buf [10] = {
+        0x80, 0x02, 0x00, 0x00, 0x00, 0x0a, 0x00, 0x00, 0x00, 0x00
+    };
+    uint8_t *buf_out = NULL;
+
+    /* prime read to successfully produce the header */
+    data->buf_size = 10;
+    will_return (__wrap_g_socket_receive, buf);
+    will_return (__wrap_g_socket_receive, 0);
+    will_return (__wrap_g_socket_receive, 0);
+    will_return (__wrap_g_socket_receive, 10);
+
+    buf_out = read_tpm_buffer_alloc (data->socket,
+                                     &data->buf_size);
+    assert_non_null (buf_out);
+    assert_int_equal (data->buf_size, TPM_HEADER_SIZE);
+    assert_memory_equal (buf_out, buf, data->buf_size);
+    g_free (buf_out);
+}
+static void
+read_tpm_buf_alloc_eof_test (void **state)
+{
+    data_t *data = *state;
+    uint8_t *buf;
+    size_t   buf_size;
+
+    /* prime read to successfully produce the header */
+    will_return (__wrap_g_socket_receive, buf_in);
+    will_return (__wrap_g_socket_receive, 0);
+    will_return (__wrap_g_socket_receive, 0);
+    will_return (__wrap_g_socket_receive, 0);
+
+    buf = read_tpm_buffer_alloc (data->socket, &buf_size);
+    assert_null (buf);
+}
+
 gint
 main (gint    argc,
       gchar  *argv[])
@@ -644,6 +711,16 @@ main (gint    argc,
                                          read_data_setup,
                                          read_data_teardown),
         cmocka_unit_test_setup_teardown (read_tpm_buf_populated_body_test,
+                                         read_data_setup,
+                                         read_data_teardown),
+        /* read_tpm_buffer_alloc*/
+        cmocka_unit_test_setup_teardown (read_tpm_buf_alloc_success_test,
+                                         read_data_setup,
+                                         read_data_teardown),
+        cmocka_unit_test_setup_teardown (read_tpm_buf_alloc_header_only_test,
+                                         read_data_setup,
+                                         read_data_teardown),
+        cmocka_unit_test_setup_teardown (read_tpm_buf_alloc_eof_test,
                                          read_data_setup,
                                          read_data_teardown),
     };

commit 6128d176729ccf5f61759d67292d76abd97f28fc

util: fix command size range check
The current check is never triggered because size_tmp cannot be both
smaller than TPM_HEADER_SIZE=10 and larger than UTIL_BUF_MAX=8192.
Instead it is supposed to check whether the command size is either too
small (does not even contain a full header) or larger than the allowed
maximum and fail accordingly.

Fixes: 665ec3b6a1fed484d58154dad257705f28524f39
Signed-off-by: Jonas Witschel <diabonas@gmx.de>

src/util.c
@@ -230,7 +230,7 @@ read_tpm_buffer_alloc (GInputStream *istream,
         switch (ret) {
         case EPROTO:
             size_tmp = get_command_size (buf);
-            if (size_tmp < TPM_HEADER_SIZE && size_tmp > UTIL_BUF_MAX) {
+            if (size_tmp < TPM_HEADER_SIZE || size_tmp > UTIL_BUF_MAX) {
                 g_warning ("%s: tpm buffer size is ouside of acceptable bounds: %zd",
                            __func__, size_tmp);
                 goto err_out;