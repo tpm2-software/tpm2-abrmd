3057c21239e59e1d5c35ba2274f8c35a8fb72726
William Roberts <william.c.roberts@intel.com>
2020-12-21 11:16:18 -0600

    ci: switch to GH actions
    Signed-off-by: William Roberts <william.c.roberts@intel.com>

---
+++ .ci/get_deps.sh
@@ -0,0 +1,28 @@
+# SPDX-License-Identifier: BSD-2
+
+set -exo pipefail
+
+pushd "$1"
+
+if [ -z "$TPM2TSS_BRANCH" ]; then
+    echo "TPM2TSS_BRANCH is unset, please specify TPM2TSS_BRANCH"
+    exit 1
+fi
+
+# Install tpm2-tss
+if [ ! -d tpm2-tss ]; then
+
+  git clone --depth=1 -b "${TPM2TSS_BRANCH}" "https://github.com/tpm2-software/tpm2-tss.git"
+  pushd tpm2-tss
+  ./bootstrap
+  ./configure --enable-debug
+  make -j$(nproc)
+  make install
+  popd
+else
+  echo "tpm2-tss already installed, skipping..."
+fi
+
+popd
+
+exit 0
