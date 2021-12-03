41fd797112f0aa7b38a34c6e80b63d311b2ff76f
malikabhi05 <abhishek.malik@intel.com>
2021-12-03 11:07:30 -0800

    Tying tss clone to tss version
    Signed-off-by: malikabhi05 <abhishek.malik@intel.com>

--- .ci/get_deps.sh
+++ .ci/get_deps.sh
@@ -3,6 +3,7 @@
 set -exo pipefail
 
 pushd "$1"
+TSS_VERSION=`git ls-remote --refs --tags https://github.com/tpm2-software/tpm2-tss.git | cut --delimiter='/' --fields=3 | tail --lines=1`
 
 if [ -z "$TPM2TSS_BRANCH" ]; then
     echo "TPM2TSS_BRANCH is unset, please specify TPM2TSS_BRANCH"
@@ -11,8 +12,13 @@ fi
 
 # Install tpm2-tss
 if [ ! -d tpm2-tss ]; then
-
-  git clone --depth=1 -b "${TPM2TSS_BRANCH}" "https://github.com/tpm2-software/tpm2-tss.git"
+  if [ -z "$GIT_FULL_CLONE" ]; then
+		echo "Doing shallow clone of tss"
+		git_extra_flags="--depth=1"
+	else
+		echo "Doing deep clone of tss"
+	fi
+  git clone $git_extra_flags -b "${TSS_VERSION}" "https://github.com/tpm2-software/tpm2-tss.git"
   pushd tpm2-tss
   ./bootstrap
   ./configure --enable-debug --disable-esys --disable-esapi --disable-fapi
